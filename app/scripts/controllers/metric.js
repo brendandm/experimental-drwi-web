dcf4627df9a4c23df2421fb55e7560022babcb48
Joshua Powell <joshua@viable.io>
2014-09-04 10:51:22 -0400

    Added new angular leaflet project to the application; Updated the menu; Added static content to site, practice and metric; Added lefalfet map to site; Updated Metric, Practice and Site controllers to include appropriate back functionality and link functionality; Removed unnecessary Navigation Controller;

--- app/scripts/controllers/metric.js
+++ app/scripts/controllers/metric.js
@@ -7,9 +7,22 @@
   .controller('MetricCtrl', function($scope, $http, $location, $log){
 
     $scope.templateUrl = '/partials/metric.html';
+    $scope.goBack = true;
 
     $scope.page = {
       name: "Metrics"
+    };
+
+    $scope.getMetric = function(practice_id) {
+      $location.url('/projects/' + $routeParams.projectId + '/sites/' + $routeParams.siteId + '/practices/' + $routeParams.practiceId);
+    };
+
+    $scope.back = function(){
+      window.history.back();
+    };
+
+    $scope.edit = function(){
+      $scope.showFields = true;
     };
 
   });
