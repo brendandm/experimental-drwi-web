71a26928a3c036c78feb57b38022c1ce760f857d
Ryan Hamley <rshamley@gmail.com>
2014-09-04 09:56:08 -0400

    commented out some dynamic stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,20 +3,20 @@
 
   angular.module('app')
 
-  .controller('MainCtrl', function($scope, $http, $location, $log, Project){
-    $scope.templateUrl = '/partials/front.html';
-    $scope.gravatar = 'http://www.gravatar.com/avatar/11800567938b3ec912c283bbc9c23938?s=80';
+    .controller('MainCtrl', function($scope, $http, $location, $log, Project){
+      $scope.templateUrl = '/partials/front.html';
+      $scope.gravatar = 'http://www.gravatar.com/avatar/11800567938b3ec912c283bbc9c23938?s=80';
 
-    $scope.page = {
-      name: "Monitoring and Assessment"
-    };
+      $scope.page = {
+        name: "Monitoring and Assessment"
+      };
 
-	var getListData = function(){
-		Project.query({}, function(data){
-			$scope.projects = data.response.features;
-			$log.log('projects', data.response.features);
-		});
-	};
+  	var getListData = function(){
+  		Project.query({}, function(data){
+  			$scope.projects = data.response.features;
+  			$log.log('projects', data.response.features);
+  		});
+  	};
 
     $scope.getProject = function(project_id){
       $location.url('/projects/' + project_id);
