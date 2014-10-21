e606f51548b90aaddf6fd95c297ee51486a4a139
Joshua Powell <joshua@viable.io>
2014-10-21 09:44:42 -0400

    Added new base Angular project with Grunt and Bower packages setup;

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practiceMonitoringAssessmentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the practiceMonitoringAssessmentApp
+ */
+angular.module('practiceMonitoringAssessmentApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practiceMonitoringAssessmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the practiceMonitoringAssessmentApp
+ */
+angular.module('practiceMonitoringAssessmentApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
