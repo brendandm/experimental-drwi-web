15efd38e925697c570b3721d66a479f672da65a3
Joshua Powell <joshua@viable.io>
2014-10-22 14:51:05 -0400

    Updated the login page to have a 'Create Account' and Help link in the header; Added site-wide hHelp link in header; Revised interior page title/pagination area to include a Create Project button and a Filter form;

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -8,7 +8,7 @@
  * Controller of the practiceMonitoringAssessmentApp
  */
 angular.module('practiceMonitoringAssessmentApp')
-  .controller('ProjectsCtrl', ['$rootScope', '$scope', '$route', 'projects', function ($rootScope, $scope, $route, projects) {
+  .controller('ProjectsCtrl', ['$rootScope', '$scope', '$route', '$routeParams', 'Feature', 'projects', function ($rootScope, $scope, $route, $routeParams, Feature, projects) {
 
     //
     // Setup basic page variables
@@ -17,6 +17,20 @@ angular.module('practiceMonitoringAssessmentApp')
       template: 'views/projects.html',
       title: 'Projects',
       back: '/',
+      links: [
+        // {
+        //   type: 'edit',
+        //   url: '/applications/' + $scope.application.id + '/edit/',
+        //   text: 'Edit this application',
+        //   static: "static"
+        // },
+        {
+          type: 'button-link new',
+          url: '/projects/new/',
+          text: 'Create project',
+          static: "static"
+        }
+      ],
       refresh: function() {
         $route.reload();
       }
@@ -24,6 +38,43 @@ angular.module('practiceMonitoringAssessmentApp')
 
     $scope.projects = projects;
 
-    console.log('$scope.projects', $scope.projects);
+
+    //
+    // Setup project filter functionality
+    //
+    $scope.project_filter = {
+      storage: 'type_061edec30db54fa0b96703b40af8d8ca',
+      field: 'project_title',
+      value: ''
+    };
+
+
+    //
+    // Search across all projects for matching criteria
+    //
+    $scope.$watch('project_filter.value', function(user_input, existing) {
+      console.log('user_input', user_input);
+      console.log('$routeParams.page', $routeParams.page);
+
+      $scope.project = Feature.query({
+        storage: $scope.project_filter.storage,
+        // page: $routeParams.page,
+        q: {
+          'filters': [
+            {
+              'name': $scope.project_filter.field,
+              'op': 'ilike',
+              'val': '%' + $scope.project_filter.value + '%'
+            }
+          ]
+        },
+        updated: new Date().getTime()
+      }).$promise.then(function(response) {
+        $scope.projects = response;
+      });
+
+      console.log('$scope.project', $scope.project);
+    });
+
 
   }]);
