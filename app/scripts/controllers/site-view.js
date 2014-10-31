e5b6647d79e88bc4f067dd097c0c1da3768d2017
Joshua Powell <joshua@viable.io>
2014-10-31 10:17:37 -0400

    Added new single Site view;

---
+++ app/scripts/controllers/site-view.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practiceMonitoringAssessmentApp.controller:SiteViewCtrl
+ * @description
+ * # SiteViewCtrl
+ * Controller of the practiceMonitoringAssessmentApp
+ */
+angular.module('practiceMonitoringAssessmentApp')
+  .controller('SiteViewCtrl', ['$rootScope', '$scope', '$route', 'user', 'users', 'template', 'fields', 'site', 'variables', function ($rootScope, $scope, $route, user, users, template, fields, site, variables) {
+
+    //
+    // Setup basic page variables
+    //
+    $rootScope.page = {
+      template: 'views/site-view.html',
+      // title: $scope.project.project_title,
+      // display_title: false,
+      // back: '/',
+      // links: [
+      //   {
+      //     text: 'Projects',
+      //     url: '/projects'
+      //   },
+      //   {
+      //     text: $scope.project.project_title,
+      //     url: '/projects/' + $scope.project.id,
+      //     type: 'active'
+      //   }
+      // ],
+      // actions: [
+      //   {
+      //     type: 'button-link new',
+      //     action: function() {
+      //       $scope.project.sites.create();
+      //     },
+      //     text: 'Create site'
+      //   }
+      // ],
+      refresh: function() {
+        $route.reload();
+      }
+    };
+
+
+  }]);
