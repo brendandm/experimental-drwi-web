d4a05e0505850002cb389e90bba451e995c1955b
Joshua Powell <joshua@viable.io>
2014-11-02 08:44:16 -0500

    Updated the Site landing page to include geographic information and a map identifying the Site's Land River Segment;

--- app/scripts/controllers/site-view.js
+++ app/scripts/controllers/site-view.js
@@ -8,7 +8,7 @@
  * Controller of the practiceMonitoringAssessmentApp
  */
 angular.module('practiceMonitoringAssessmentApp')
-  .controller('SiteViewCtrl', ['$rootScope', '$scope', '$route', '$location', 'user', 'Template', 'Feature', 'template', 'fields', 'project', 'site', 'storage', 'variables', function ($rootScope, $scope, $route, $location, user, Template, Feature, template, fields, project, site, storage, variables) {
+  .controller('SiteViewCtrl', ['$rootScope', '$scope', '$route', '$location', 'user', 'Site', 'Template', 'Feature', 'template', 'fields', 'project', 'site', 'storage', 'variables', 'leafletData', function ($rootScope, $scope, $route, $location, user, Site, Template, Feature, template, fields, project, site, storage, variables, leafletData) {
 
     //
     // Assign project to a scoped variable
@@ -61,6 +61,254 @@ angular.module('practiceMonitoringAssessmentApp')
       }
     };
 
+    var featureGroup = new L.FeatureGroup();
+
+    $scope.map = {
+      defaults: {
+        scrollWheelZoom: false,
+        zoomControl: false,
+        maxZoom: 19
+      },
+      layers: {
+        baselayers: {
+          basemap: {
+            name: 'Satellite Imagery',
+            url: 'https://{s}.tiles.mapbox.com/v3/' + Site.settings.services.mapbox.satellite + '/{z}/{x}/{y}.png',
+            type: 'xyz',
+            layerOptions: {
+              attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a>'
+            }
+          }
+        }
+      },
+      center: {
+        lat: ($scope.site.geometry !== null && $scope.site.geometry !== undefined) ? $scope.site.geometry.geometries[0].coordinates[1] : 38.362,
+        lng: ($scope.site.geometry !== null && $scope.site.geometry !== undefined) ? $scope.site.geometry.geometries[0].coordinates[0] : -81.119, 
+        zoom: 6
+      },
+      markers: {
+        LandRiverSegment: {
+          lat: ($scope.site.geometry !== null && $scope.site.geometry !== undefined) ? $scope.site.geometry.geometries[0].coordinates[1] : 38.362,
+          lng: ($scope.site.geometry !== null && $scope.site.geometry !== undefined) ? $scope.site.geometry.geometries[0].coordinates[0] : -81.119, 
+          focus: false,
+          draggable: true,
+          icon: {
+            iconUrl: '//api.tiles.mapbox.com/v4/marker/pin-l+b1c11d.png?access_token=' + Site.settings.services.mapbox.access_token,
+            iconRetinaUrl: '//api.tiles.mapbox.com/v4/marker/pin-l+b1c11d@2x.png?access_token=' + Site.settings.services.mapbox.access_token,
+            iconSize: [38, 90],
+            iconAnchor: [18, 44],
+            popupAnchor: [0, 0]
+          }
+        }
+      }
+    };
+
+    //
+    // Convert a GeoJSON Feature Collection to a valid Leaflet Layer
+    //
+    $scope.geojsonToLayer = function (geojson, layer) {
+      layer.clearLayers();
+      function add(l) {
+        l.addTo(layer);
+      }
+
+      //
+      // Make sure the GeoJSON object is added to the layer with appropriate styles
+      //
+      L.geoJson(geojson, {
+        style: {
+          stroke: true,
+          fill: false,
+          weight: 2,
+          opacity: 1,
+          color: 'rgb(255,255,255)',
+          lineCap: 'square'
+        }
+      }).eachLayer(add);
+    };
+
+    $scope.geolocation = {
+      drawSegment: function(geojson) {
+          
+        leafletData.getMap().then(function(map) {
+          //
+          // Reset the FeatureGroup because we don't want multiple parcels drawn on the map
+          //
+          map.removeLayer(featureGroup);
+
+          //
+          // Convert the GeoJSON to a layer and add it to our FeatureGroup
+          //
+          $scope.geojsonToLayer(geojson, featureGroup);
+
+          //
+          // Add the FeatureGroup to the map
+          //
+          map.addLayer(featureGroup);
+
+          //
+          // If we can getBounds then we can zoom to a specific level, we need to check to see
+          // if the FeatureGroup has any bounds first though, otherwise we'll get an error.
+          //
+          var bounds = featureGroup.getBounds();
+
+          if (bounds.hasOwnProperty('_northEast')) {
+            map.fitBounds(featureGroup.getBounds());
+          }
+        });
+
+      },
+      getSegment: function(coordinates) {
+
+        $http({
+          method: 'GET', 
+          url: '//api.commonscloud.org/v2/type_f9d8609090494dac811e6a58eb8ef4be/intersects.geojson',
+          params: {
+            geometry: coordinates.lng + ' ' + coordinates.lat
+          }
+        }).
+          success(function(data, status, headers, config) {
+            console.log('LandRiverSegment Request', data);
+            $scope.geolocation.drawSegment(data);
+            if (data.features.length > 0) {
+              $scope.site.type_f9d8609090494dac811e6a58eb8ef4be = [];
+              $scope.site.type_f9d8609090494dac811e6a58eb8ef4be.push(data.features[0].properties);
+            }
+          }).
+          error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log('error', data);
+
+            return data;
+          });
+      },
+      search: function() {
+        $timeout.cancel(timeout);
+
+        timeout = $timeout(function () {
+          $scope.geolocation.initGeocoder();
+        }, 800);
+      },
+      initGeocoder: function() {
+        var requested_location = $scope.site.geolocation;
+
+        if (requested_location.length >= 3) {
+          var geocode_service_url = '//api.tiles.mapbox.com/v4/geocode/mapbox.places-v1/' + requested_location + '.json';
+          $http({
+            method: 'get',
+            url: geocode_service_url,
+            params: {
+              'callback': 'JSON_CALLBACK',
+              'access_token': Site.settings.services.mapbox.access_token
+            },
+            headers: {
+              'Authorization': 'external'
+            }
+          }).success(function(data) {
+            $scope.geocode_features = data.features;
+          }).error(function(data, status, headers, config) {
+            console.log('ERROR: ', data);
+          });
+        }
+      },
+      select: function(geocode) {
+
+        $scope.site.site_city = geocode.place_name;
+
+        //
+        // Move the draggable marker to the newly selected address
+        //
+        $scope.map.markers.LandRiverSegment = {
+          lat: geocode.center[1],
+          lng: geocode.center[0], 
+          focus: false,
+          draggable: true,
+          icon: {
+            iconUrl: '//api.tiles.mapbox.com/v4/marker/pin-l+b1c11d.png?access_token=' + Site.settings.services.mapbox.access_token,
+            iconRetinaUrl: '//api.tiles.mapbox.com/v4/marker/pin-l+b1c11d@2x.png?access_token=' + Site.settings.services.mapbox.access_token,
+            iconSize: [38, 90],
+            iconAnchor: [18, 44],
+            popupAnchor: [0, 0]
+          }
+        };
+
+        //
+        // Center the map view on the newly selected address
+        //
+        $scope.map.center = {
+          lat: geocode.center[1],
+          lng: geocode.center[0], 
+          zoom: 12
+        };
+
+        //
+        // Get the parcel for the property if one exists
+        //
+        $scope.geolocation.getSegment($scope.map.center);
+
+        //
+        // Since an address has been select, we should clear the drop down so the user
+        // can focus on the map.
+        //
+        $scope.geocode_features = [];
+
+        //
+        // We should also make sure we save this information to the
+        // User's Site object, that way if we come back later it is
+        // retained within the system
+        //
+        $scope.site.geometry = {
+          type: 'GeometryCollection',
+          geometries: []
+        };
+        $scope.site.geometry.geometries.push(geocode.geometry);
+
+
+        $timeout(function () {
+          leafletData.getMap().then(function(map) {
+            map.invalidateSize();
+          });
+        }, 200);
+
+      }
+    };
+    
+
+
+    //
+    // Define our map interactions via the Angular Leaflet Directive
+    //
+    leafletData.getMap().then(function(map) {
+
+      //
+      // Move Zoom Control position to bottom/right
+      //
+      new L.Control.Zoom({
+        position: 'bottomright'
+      }).addTo(map);
+
+      //
+      // Listen for when the user drops the pin on a new geography
+      //
+      $scope.$on('leafletDirectiveMarker.dragend', function(event, args) {
+        $scope.geolocation.getSegment(args.leafletEvent.target._latlng);
+
+        $scope.site.geometry = {
+          type: 'GeometryCollection',
+          geometries: []
+        };
+        $scope.site.geometry.geometries.push({
+          type: 'Point',
+          coordinates: [
+            args.leafletEvent.target._latlng.lng,
+            args.leafletEvent.target._latlng.lat
+          ]
+        });
+
+
+      });
+    });
 
     //
     // Determine whether the Edit button should be shown to the user. Keep in mind, this doesn't effect
@@ -97,6 +345,19 @@ angular.module('practiceMonitoringAssessmentApp')
         }
 
       });
+    }
+
+    //
+    // If the page is being loaded, and a parcel exists within the user's plan, that means they've already
+    // selected their property, so we just need to display it on the map for them again.
+    //
+    if ($scope.site.type_f9d8609090494dac811e6a58eb8ef4be.length > 0) {
+      var json = $scope.site.type_f9d8609090494dac811e6a58eb8ef4be[0];
+      var geojson = {
+        type: 'Feature',
+        geometry: json.geometry
+      };
+      $scope.geolocation.drawSegment(geojson);
     }
 
   }]);
