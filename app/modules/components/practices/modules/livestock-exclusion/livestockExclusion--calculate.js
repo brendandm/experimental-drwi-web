2e885774d1539d069d7d7f098c7f7a5de9495f4a
Joshua Powell <joshua@viable.io>
2015-04-27 10:54:08 -0400

    Added Installation Summary for the Livestock Exclusion;

--- app/modules/components/practices/modules/livestock-exclusion/livestockExclusion--calculate.js
+++ app/modules/components/practices/modules/livestock-exclusion/livestockExclusion--calculate.js
@@ -15,15 +15,43 @@ angular.module('practiceMonitoringAssessmentApp')
       totalDaysPerYearInStream: function(values) {
         return values.instream_dpmjan+values.instream_dpmfeb+values.instream_dpmmar+values.instream_dpmapr+values.instream_dpmmay+values.instream_dpmjun+values.instream_dpmjul+values.instream_dpmaug+values.instream_dpmsep+values.instream_dpmoct+values.instream_dpmnov+values.instream_dpmdec; 
       },
-      totalHoursPerYearInStream: function(values) {
+      averageHoursPerYearInStream: function(values) {
         var totalHoursPerYearInStream = values.instream_hpdjan+values.instream_hpdfeb+values.instream_hpdmar+values.instream_hpdapr+values.instream_hpdmay+values.instream_hpdjun+values.instream_hpdjul+values.instream_hpdaug+values.instream_hpdsep+values.instream_hpdoct+values.instream_hpdnov+values.instream_hpddec; 
         return (totalHoursPerYearInStream/12);
       },
       averageDaysPerYearInStream: function(values) {
-        var dpm = this.totalDaysPerYearInStream(values);
-        var hpd = this.totalHoursPerYearInStream(values);
-        console.log('averageDaysPerYearInStream', dpm, '*', hpd, '/', 24);
+        var dpm = this.totalDaysPerYearInStream(values),
+            hpd = this.averageHoursPerYearInStream(values);
         return (dpm*hpd/24);
+      },
+      quantityInstalled: function(values, field, format) {
+
+        var planned_total = 0,
+            installed_total = 0,
+            percentage = 0;
+
+        // Get readings organized by their Type
+        angular.forEach(values, function(reading, $index) {
+
+          if (reading.measurement_period === 'Planning') {
+            planned_total += reading[field];
+          } else if (reading.measurement_period === 'Installation') {
+            installed_total += reading[field];
+          }
+
+        });
+
+        // Divide the Installed Total by the Planned Total to get a percentage of installed
+        if (planned_total >= 1) {
+          if (format === '%') {
+            percentage = (installed_total/planned_total);
+            return (percentage*100);
+          } else {
+            return installed_total;
+          }
+        }
+
+        return null;
       }
     };
   }]);
