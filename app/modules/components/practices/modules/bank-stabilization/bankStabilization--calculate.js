e057486a4074eb5c9e59faa718bac82f8d446400
Joshua Powell <joshua@viable.io>
2015-09-20 09:00:06 -0400

    Added additional installation informaiton to the Bank Stabilization Report; Added Chesapeake Bay Metrics for the Bank Stabilization report;

--- app/modules/components/practices/modules/bank-stabilization/bankStabilization--calculate.js
+++ app/modules/components/practices/modules/bank-stabilization/bankStabilization--calculate.js
@@ -8,6 +8,38 @@
 angular.module('practiceMonitoringAssessmentApp')
   .service('BankStabilizationCalculate', function() {
     return {
+      milesStreambankRestored: function(value) {
+        return (value.installation_length_of_streambank/5280);
+      },
+      milesStreambankInstalledFromPlan: function(values, format) {
+
+        var planned_total = 0,
+            installed_total = 0,
+            percentage = 0;
+
+        // Get readings organized by their Type
+        angular.forEach(values, function(reading, $index) {
+
+          if (reading.measurement_period === 'Planning') {
+            planned_total += (reading.installation_length_of_streambank/5280);
+          } else if (reading.measurement_period === 'Installation') {
+            installed_total += (reading.installation_length_of_streambank/5280);
+            console.log('installed_total', (reading.installation_length_of_streambank/5280))
+          }
+
+        });
+
+        // Divide the Installed Total by the Planned Total to get a percentage of installed
+        if (format === '%') {
+          percentage = (installed_total/planned_total);
+          return (percentage*100);
+        } else {
+          return installed_total;
+        }
+
+        return 0;
+
+      },
       quantityInstalled: function(values, field, format) {
 
         var planned_total = 0,
