1cbf7b835df41bf68ec8efa8e803c343971258a2
Joshua Powell <joshua@viable.io>
2015-10-26 18:04:14 -0400

    Added empty Calculator for EHR;'

---
+++ app/modules/components/practices/modules/enhanced-stream-restoration/enhancedStreamRestoration--calculate.js
@@ -0,0 +1,16 @@
+(function () {
+
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name
+   * @description
+   */
+  angular.module('practiceMonitoringAssessmentApp')
+    .service('EnhancedStreamRestorationCalculate', function() {
+      return {
+      };
+    });
+
+}());
