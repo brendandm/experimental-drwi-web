eedd7b8e8994d5d7d9f779fce41e4a0e579aeab5
Joshua Powell <joshua@viable.io>
2015-10-29 09:18:27 -0400

    Updated Ehanced Stream Restoration report controller;

---
+++ app/modules/components/practices/modules/enhanced-stream-restoration/enhancedStreamRestoration--report.js
@@ -0,0 +1,16 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name
+   * @description
+   */
+  angular.module('practiceMonitoringAssessmentApp')
+    .controller('EnhancedStreamRestorationReportController', function ($rootScope, $scope, $route, $location, $timeout, $http, $q, user, Template, Feature, template, fields, project, site, practice, readings, commonscloud, Storage, Landuse, CalculateBioretention, Calculate, StateLoad) {
+
+
+    });
+
+}());
