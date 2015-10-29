63020feb8894a65f978543970449afefc680c960
Joshua Powell <joshua@viable.io>
2015-10-29 09:17:39 -0400

    Updated Ehanced Stream Restoration form controller;

---
+++ app/modules/components/practices/modules/enhanced-stream-restoration/enhancedStreamRestoration--form.js
@@ -0,0 +1,16 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name
+   * @description
+   */
+  angular.module('practiceMonitoringAssessmentApp')
+    .controller('EnhancedStreamRestorationFormController', function ($rootScope, $scope, $route, $location, user, Template, Field, Feature, Storage, template, project, site, practice, commonscloud) {
+
+      
+    });
+
+}());
