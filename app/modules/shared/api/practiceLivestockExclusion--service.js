24ab8b2c5223f0b045ab9b3e0c08ad23c37bf2be
Joshua Powell <joshua@viable.io>
2016-02-13 17:30:18 -0500

    Added Livestock Exclusion API service;

---
+++ app/modules/shared/api/practiceLivestockExclusion--service.js
@@ -0,0 +1,28 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name
+   * @description
+   */
+  angular.module('FieldStack')
+    .service('PracticeLivestockExclusion', function (environment, Preprocessors, $resource) {
+      return $resource(environment.apiUrl.concat('/v1/data/bmp-livestock-exclusion/:id'), {
+        'id': '@id'
+      }, {
+        'query': {
+          isArray: false
+        },
+        'update': {
+          method: 'PATCH',
+          transformRequest: function(data) {
+            var feature = Preprocessors.geojson(data);
+            return angular.toJson(feature);
+          }
+        }
+      });
+    });
+
+}());
