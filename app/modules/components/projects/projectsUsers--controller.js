60fd04ac334fb8684c4964701e3f7dd77d5434ac
Joshua Powell <joshua@viable.io>
2016-02-26 15:52:23 -0500

    Upgrade additional collaborator functionality

--- app/modules/components/projects/projectsUsers--controller.js
+++ app/modules/components/projects/projectsUsers--controller.js
@@ -86,13 +86,6 @@
       });
 
       //
-      // Setup the Project
-      //
-      // $scope.project.users = projectUsers;
-      // $scope.project.users_edit = false;
-
-
-      //
       // Modal Windows
       //
       self.modals = {
@@ -110,7 +103,6 @@
           }
         }
       };
-
 
       self.users = {
         list: members,
@@ -140,78 +132,15 @@
             });
           });
         },
-        remove: function(user) {
-          var index = self.project.users.features.indexOf(user);
-
-          Feature.RemoveUser({
-              storage: storage,
-              featureId: self.project.id,
-              userId: user.id
-            }).then(function(response) {
-              //
-              // Once the users have been added to the project, close the modal
-              // and refresh the page
-              //
-              self.project.users.features.splice(index, 1);
-            });
+        remove: function() {
+          self.project.$update().then(function(response) {
+            $route.reload();
+          }).then(function(error) {
+            // Do something with the error
+          });
         },
         remove_confirm: false
       };
-
-
-      $scope.invite = [];
-
-
-      // //
-      // // Setup basic page variables
-      // //
-      // $rootScope.page = {
-      //   template: '/modules/components/projects/views/projects--users.html',
-      //   title: $scope.project.project_title + ' Users',
-      //   display_title: false,
-      //   editable: true,
-      //   back: '/',
-      //   links: [
-      //     {
-      //       text: 'Projects',
-      //       url: '/projects'
-      //     },
-      //     {
-      //       text: $scope.project.project_title,
-      //       url: '/projects/' + $scope.project.id
-      //     },
-      //     {
-      //       text: 'Collaborators',
-      //       url: '/projects/' + $scope.project.id + '/users',
-      //       type: 'active'
-      //     }
-      //   ],
-      //   actions: [
-      //     {
-      //       type: 'button-link',
-      //       // url: '/projects/' + $scope.project.id + '/users/invite',
-      //       action: function($index) {
-      //         $scope.project.users_edit = ! $scope.project.users_edit;
-      //         $scope.page.actions[$index].visible = ! $scope.page.actions[$index].visible;
-      //       },
-      //       visible: false,
-      //       text: 'Edit collaborators',
-      //       alt: 'Done Editing'
-      //     },
-      //     {
-      //       type: 'button-link new',
-      //       // url: '/projects/' + $scope.project.id + '/users/invite',
-      //       action: function() {
-      //         console.log('modal');
-      //         $scope.modals.open('inviteUser');
-      //       },
-      //       text: 'Add a collaborator'
-      //     }
-      //   ],
-      //   refresh: function() {
-      //     $route.reload();
-      //   }
-      // };
 
     });
 
