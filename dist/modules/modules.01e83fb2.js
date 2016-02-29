5acade64e1368775bb9d6f0337012205722fa4b0
Joshua Powell <joshuapowell@users.noreply.github.com>
2016-02-29 09:04:58 -0500

    Update modules.01e83fb2.js

--- dist/modules/modules.01e83fb2.js
+++ dist/modules/modules.01e83fb2.js
@@ -46,7 +46,7 @@ angular.module('FieldStack')
 
  angular.module('config', [])
 
-.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',siteUrl:'http://127.0.0.1:9000',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'})
+.constant('environment', {name:'local',apiUrl:'http://api.fieldstack.io',siteUrl:'http://www.fieldstack.io',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'})
 
 ;
 (function() {
