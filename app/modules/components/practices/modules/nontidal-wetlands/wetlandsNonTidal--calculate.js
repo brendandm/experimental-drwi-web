17c374504977cee97049f14587c2ea48ec0888b4
Joshua Powell <joshua@viable.io>
2016-03-21 15:12:45 -0400

    Added Planned load reductions for Urban areas

--- app/modules/components/practices/modules/nontidal-wetlands/wetlandsNonTidal--calculate.js
+++ app/modules/components/practices/modules/nontidal-wetlands/wetlandsNonTidal--calculate.js
@@ -11,7 +11,8 @@
     .service('CalculateWetlandsNonTidal', function(Calculate, LoadData, $q) {
       return {
         loads: {
-          preinstallation: {}
+          preinstallation: {},
+          planned: {}
         },
         loadDataQuery: function(landuse, segment) {
           console.log('loadDataQuery', landuse, segment);
@@ -52,13 +53,11 @@
                 'installation_upland_landuse_1',
                 'installation_upland_landuse_2',
                 'installation_upland_landuse_3',
-                'installation_upland_landuse_4',
+                'installation_upland_landuse_4'
               ];
 
           for (var i = 0; i < landuseList.length; i++) {
-            var _landuse = landuseList[i];
-            console.log('getLoadDataQueries', value.properties[_landuse], segment);
-            savedQueries.push(self.loadDataQuery(value.properties[_landuse], segment));
+            savedQueries.push(self.loadDataQuery(value.properties[landuseList[i]], segment));
           }
 
           return savedQueries;
@@ -80,6 +79,20 @@
 
           return calculatedLoads.reduce(this.reduceLoadValues);
         },
+        plannedLoad: function(data, loads, efficiency, parameter) {
+
+          var landuses = 4,
+              calculatedLoads = [];
+
+          for (var i = 0; i < landuses; i++) {
+            var landuse = 'installation_upland_landuse_'+(i+1),
+                acresTreated = 'installation_landuse_acreage_'+(i+1);
+
+            calculatedLoads.push(data.properties[acresTreated]*loads[i][parameter]*efficiency[parameter]);
+          };
+
+          return calculatedLoads.reduce(this.reduceLoadValues);
+        },
         preInstallationLoads: function(reports, segment) {
 
           var self = this,
@@ -87,11 +100,35 @@
               savedQueries = this.getLoadDataQueries(planningData, segment);
 
           $q.all(savedQueries).then(function(successResponse) {
+
+            var loadData = successResponse,
+                efficiency = {
+                  nitrogen: 0.20,
+                  phosphorus: 0.45,
+                  sediment: 0.60
+                };
+
             self.loads.preinstallation = {
-              nitrogen: self.preInstallationLoad(planningData, successResponse, 'nitrogen'),
-              phosphorus: self.preInstallationLoad(planningData, successResponse, 'phosphorus'),
-              sediment: self.preInstallationLoad(planningData, successResponse, 'sediment')
+              nitrogen: self.preInstallationLoad(planningData, loadData, 'nitrogen'),
+              phosphorus: self.preInstallationLoad(planningData, loadData, 'phosphorus'),
+              sediment: self.preInstallationLoad(planningData, loadData, 'sediment')
             };
+
+            switch (planningData.properties.planning_project_type) {
+              case 'urban':
+                self.loads.planned = {
+                  nitrogen: self.plannedLoad(planningData, loadData, efficiency, 'nitrogen'),
+                  phosphorus: self.plannedLoad(planningData, loadData, efficiency, 'phosphorus'),
+                  sediment: self.plannedLoad(planningData, loadData, efficiency, 'sediment')
+                }
+              case 'agriculture':
+                self.loads.planned = {
+                  nitrogen: self.plannedLoad(planningData, loadData, efficiency, 'nitrogen'),
+                  phosphorus: self.plannedLoad(planningData, loadData, efficiency, 'phosphorus'),
+                  sediment: self.plannedLoad(planningData, loadData, efficiency, 'sediment')
+                }
+            }
+
           }, function(errorResponse) {
             console.log('errorResponse', errorResponse);
           });
