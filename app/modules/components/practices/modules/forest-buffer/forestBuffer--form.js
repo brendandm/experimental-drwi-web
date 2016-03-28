a88c4efc58774b076561dada4ebe068245b9e2e0
Joshua Powell <joshua@viable.io>
2016-03-28 10:30:00 -0400

    Added new report date, measurement period, and WIP reporting field styles, new landuse drop downs for Forest Buffer

--- app/modules/components/practices/modules/forest-buffer/forestBuffer--form.js
+++ app/modules/components/practices/modules/forest-buffer/forestBuffer--form.js
@@ -24,6 +24,42 @@
 
       self.landuse = landuse;
 
+      //
+      // Setup all of our basic date information so that we can use it
+      // throughout the page
+      //
+      self.today = new Date();
+
+      self.days = [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday'
+      ];
+
+      self.months = [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+      ];
+
+      function parseISOLike(s) {
+          var b = s.split(/\D/);
+          return new Date(b[0], b[1]-1, b[2]);
+      }
+
       practice.$promise.then(function(successResponse) {
 
         self.practice = successResponse;
@@ -48,6 +84,20 @@
           //
           report.$promise.then(function(successResponse) {
             self.report = successResponse;
+
+            if (self.report.properties.report_date) {
+                self.today = parseISOLike(self.report.properties.report_date);
+            }
+
+            //
+            // Check to see if there is a valid date
+            //
+            self.date = {
+                month: self.months[self.today.getMonth()],
+                date: self.today.getDate(),
+                day: self.days[self.today.getDay()],
+                year: self.today.getFullYear()
+            };
 
             $rootScope.page.title = self.practice.properties.practice_type;
             $rootScope.page.links = [
@@ -98,7 +148,20 @@
         }
       });
 
+      $scope.$watch(angular.bind(this, function() {
+          return this.date;
+      }), function (response) {
+          if (response) {
+              var _new = response.month + ' ' + response.date + ' ' + response.year,
+              _date = new Date(_new);
+              self.date.day = self.days[_date.getDay()];
+          }
+      }, true);
+
       self.saveReport = function() {
+
+        self.report.properties.report_date = self.date.month + ' ' + self.date.date + ' ' + self.date.year;
+
         self.report.$update().then(function(successResponse) {
           $location.path('/projects/' + projectId + '/sites/' + siteId + '/practices/' + practiceId + '/' + self.practiceType);
         }, function(errorResponse) {
