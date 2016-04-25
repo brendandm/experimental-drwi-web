c23607c50dc6c26736335f7c71daf77873a80f6f
Joshua Powell <joshua@viable.io>
2016-04-25 13:56:58 -0400

    Version 1.2.2 Release

--- dist/modules/modules.1c4914d2.js
+++ dist/modules/modules.1c4914d2.js
@@ -47,9 +47,14 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'staging',apiUrl:'http://stg.api.fielddoc.org',siteUrl:'http://stg.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'})
+.constant('environment', {            name: 'production',
+            apiUrl: 'https://api.fielddoc.org',
+            siteUrl: 'https://www.fielddoc.org',
+            clientId: 'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'
+})
 
 ;
+
 /**
  * angular-save2pdf - angular jsPDF wrapper
  * Copyright (c) 2015 John Daily Jr.,
@@ -2351,101 +2356,6 @@ angular.module('FieldDoc')
 
     });
 
-  });
-
-'use strict';
-
-/**
- * @ngdoc service
- * @name.
- * @description
- */
-angular.module('FieldDoc')
-  .constant('AnimalType', {
-      beef: {
-        average_weight: 877.19,
-        manure: 58,
-        total_nitrogen: 0.0059,
-        total_phosphorus: 0.0016,
-      },
-      dairy: {
-        average_weight: 1351.35,
-        manure: 86,
-        total_nitrogen: 0.0052,
-        total_phosphorus: 0.001,
-      },
-      'other cattle': {
-        average_weight: 480.77,
-        manure: 64.39,
-        total_nitrogen: 0.0037,
-        total_phosphorus: 0.001,
-      },
-      broilers: {
-        average_weight: 2.2,
-        manure: 85,
-        total_nitrogen: 0.0129,
-        total_phosphorus: 0.0035
-      },
-      layers: {
-        average_weight: 4,
-        manure: 64,
-        total_nitrogen: 0.0131,
-        total_phosphorus: 0.0047
-      },
-      pullets: {
-        average_weight: 2.84,
-        manure: 45.56,
-        total_nitrogen: 0.0136,
-        total_phosphorus: 0.0053
-      },
-      turkeys: {
-        average_weight: 14.93,
-        manure: 47,
-        total_nitrogen: 0.0132,
-        total_phosphorus: 0.0049
-      },
-      'hogs and pigs for breeding': {
-        average_weight: 374.53,
-        manure: 33.46,
-        total_nitrogen: 0.0066,
-        total_phosphorus: 0.0021
-      },
-      'hogs for slaughter': {
-        average_weight: 110.01,
-        manure: 84,
-        total_nitrogen: 0.0062,
-        total_phosphorus: 0.0021
-      },
-      horses: {
-        average_weight: 1000,
-        manure: 51,
-        total_nitrogen: 0.0059,
-        total_phosphorus: 0.0014
-      },
-      'angora goats': {
-        average_weight: 65.02,
-        manure: 41,
-        total_nitrogen: 0.011,
-        total_phosphorus: 0.0027
-      },
-      'milk goats': {
-        average_weight: 65.02,
-        manure: 41,
-        total_nitrogen: 0.011,
-        total_phosphorus: 0.0027
-      },
-      'sheep and lambs': {
-        average_weight: 100,
-        manure: 40,
-        total_nitrogen: 0.0105,
-        total_phosphorus: 0.0022
-      },
-      biosolids: {
-        average_weight: null,
-        manure: null,
-        total_nitrogen: 0.039,
-        total_phosphorus: 0.025
-      }
   });
 
 'use strict';
