170e9276e4c535a88e78b0c96f2e18b34cb91f42
joshuapowell <joshua@viable.io>
2017-03-01 10:46:20 -0500

    Added Agriculture Generic practice

---
+++ app/modules/components/practices/modules/agriculture-generic/agricultureGeneric--calculate.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name FieldDoc.Storage
+ * @description
+ * Service in the FieldDoc.
+ */
+angular.module('FieldDoc')
+  .service('CalculateAgricultureGeneric', function(Calculate, EfficiencyAgricultureGeneric, $q) {
+
+    return {};
+
+  });
---
+++ app/modules/shared/api/efficiencyAgricultureGeneric--service.js
@@ -0,0 +1,28 @@
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name
+     * @description
+     */
+    angular.module('FieldDoc')
+      .service('EfficiencyAgricultureGeneric', function (environment, Preprocessors, $resource) {
+        return $resource(environment.apiUrl.concat('/v1/data/efficiency-agriculture-generic/:id'), {
+          'id': '@id'
+        }, {
+          'query': {
+            'isArray': false
+          },
+          'update': {
+            'method': 'PATCH',
+            transformRequest: function(data) {
+              var feature = Preprocessors.geojson(data);
+              return angular.toJson(feature);
+            }
+          }
+        });
+      });
+
+  }());
