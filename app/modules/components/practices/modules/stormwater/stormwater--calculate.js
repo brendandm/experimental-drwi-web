57453493af498990069aa1641491a8dcfad2b03c
joshuapowell <joshua@viable.io>
2017-03-16 16:02:50 -0400

    Added Stormwater Calculations for Adjustor curves; Added Stormwater Calculator for some Chesapeake Bay Metrics; Added custom fields for Planning and Installation

--- app/modules/components/practices/modules/stormwater/stormwater--calculate.js
+++ app/modules/components/practices/modules/stormwater/stormwater--calculate.js
@@ -11,7 +11,133 @@ angular.module('FieldDoc')
 
     return {
       readings: null,
-      loadData: null
+      loadData: null,
+      plannedRunoffReductionAdjustorCurveNitrogen: function(format) {
+
+        var depthTreated = 1.0,
+            first = 0.0308*Math.pow(depthTreated, 5),
+            second = 0.2562*Math.pow(depthTreated, 4),
+            third = 0.8634*Math.pow(depthTreated, 3),
+            fourth = 1.5285*Math.pow(depthTreated, 2),
+            fifth = 1.501*depthTreated,
+            reduction = (first-second+third-fourth+fifth-0.013);
+
+        return (format === '%') ? reduction*100 : reduction;
+      },
+      plannedRunoffReductionAdjustorCurvePhosphorus: function(format) {
+
+        var depthTreated = 1.0,
+            first = 0.0304*Math.pow(depthTreated, 5),
+            second = 0.2619*Math.pow(depthTreated, 4),
+            third = 0.9161*Math.pow(depthTreated, 3),
+            fourth = 1.6837*Math.pow(depthTreated, 2),
+            fifth = 1.7072*depthTreated,
+            reduction = (first-second+third-fourth+fifth-0.0091);
+
+        return (format === '%') ? reduction*100 : reduction;
+      },
+      plannedRunoffReductionAdjustorCurveSediment: function(format) {
+
+        var depthTreated = 1.0,
+            first = 0.0326*Math.pow(depthTreated, 5),
+            second = 0.2806*Math.pow(depthTreated, 4),
+            third = 0.9816*Math.pow(depthTreated, 3),
+            fourth = 1.8039*Math.pow(depthTreated, 2),
+            fifth = 1.8292*depthTreated,
+            reduction = (first-second+third-fourth+fifth-0.0098);
+
+        return (format === '%') ? reduction*100 : reduction;
+      },
+      plannedStormwaterTreatmentAdjustorCurveNitrogen: function(format) {
+
+        var depthTreated = 1.0,
+            first = 0.0152*Math.pow(depthTreated, 5),
+            second = 0.131*Math.pow(depthTreated, 4),
+            third = 0.4581*Math.pow(depthTreated, 3),
+            fourth = 0.8418*Math.pow(depthTreated, 2),
+            fifth = 0.8536*depthTreated,
+            reduction = (first-second+third-fourth+fifth-0.0046);
+
+        return (format === '%') ? reduction*100 : reduction;
+      },
+      plannedStormwaterTreatmentAdjustorCurvePhosphorus: function(format) {
+
+        var depthTreated = 1.0,
+            first = 0.0239*Math.pow(depthTreated, 5),
+            second = 0.2058*Math.pow(depthTreated, 4),
+            third = 0.7198*Math.pow(depthTreated, 3),
+            fourth = 1.3229*Math.pow(depthTreated, 2),
+            fifth = 1.3414*depthTreated,
+            reduction = (first-second+third-fourth+fifth-0.0072);
+
+        return (format === '%') ? reduction*100 : reduction;
+      },
+      plannedStormwaterTreatmentAdjustorCurveSediment: function(format) {
+
+        var depthTreated = 1.0,
+            first = 0.0304*Math.pow(depthTreated, 5),
+            second = 0.2619*Math.pow(depthTreated, 4),
+            third = 0.9161*Math.pow(depthTreated, 3),
+            fourth = 1.6837*Math.pow(depthTreated, 2),
+            fifth = 1.7072*depthTreated,
+            reduction = (first-second+third-fourth+fifth-0.0091);
+
+        return (format === '%') ? reduction*100 : reduction;
+      },
+      prePlannedNitrogenLoad: function(imperviousArea, drainageArea, loadData) {
+        return ((imperviousArea*loadData.impervious.tn_ual) + (drainageArea-imperviousArea) * (loadData.pervious.tn_ual))/43560;
+      },
+      prePlannedPhosphorusLoad: function(imperviousArea, drainageArea, loadData) {
+        return ((imperviousArea*loadData.impervious.tp_ual) + (drainageArea-imperviousArea) * (loadData.pervious.tp_ual))/43560;
+      },
+      prePlannedSedimentLoad: function(imperviousArea, drainageArea, loadData) {
+        return ((imperviousArea*loadData.impervious.tss_ual) + (drainageArea-imperviousArea) * (loadData.pervious.tss_ual))/43560;
+      },
+      plannedNitrogenReduction: function(imperviousArea, drainageArea, loadData, func) {
+
+        //
+        // NEED TO KNOW IF THE PROJECT IS `RUNOFF REDUCTION` or `STROMWATER
+        // TREATMENT` SO THAT WE CAN SELECT THE RIGHT ADJUSTOR CURVE %
+        //
+
+        var self = this,
+            prePlannedReductionNitrogen = self.prePlannedNitrogenLoad(imperviousArea, drainageArea, loadData),
+            plannedAdjustorCurveNitrogen = self[func](); // @TODO Come back to this and fill in the dynamic adjustor curve value
+
+        return (prePlannedReductionNitrogen*plannedAdjustorCurveNitrogen);
+      },
+      plannedPhosphorusReduction: function(imperviousArea, drainageArea, loadData, func) {
+
+        //
+        // NEED TO KNOW IF THE PROJECT IS `RUNOFF REDUCTION` or `STROMWATER
+        // TREATMENT` SO THAT WE CAN SELECT THE RIGHT ADJUSTOR CURVE %
+        //
+
+        var self = this,
+            prePlannedReduction = self.prePlannedPhosphorusLoad(imperviousArea, drainageArea, loadData),
+            plannedAdjustorCurve = self[func](); // @TODO Come back to this and fill in the dynamic adjustor curve value
+
+        return (prePlannedReduction*plannedAdjustorCurve);
+      },
+      plannedSedimentReduction: function(imperviousArea, drainageArea, loadData, func) {
+
+        //
+        // NEED TO KNOW IF THE PROJECT IS `RUNOFF REDUCTION` or `STROMWATER
+        // TREATMENT` SO THAT WE CAN SELECT THE RIGHT ADJUSTOR CURVE %
+        //
+
+        var self = this,
+            prePlannedReduction = self.prePlannedSedimentLoad(imperviousArea, drainageArea, loadData),
+            plannedAdjustorCurve = self[func](); // @TODO Come back to this and fill in the dynamic adjustor curve value
+
+        return (prePlannedReduction*plannedAdjustorCurve);
+      },
+      metricTotalPracticeArea: function(_report) {
+        return _report.properties.practice_1_extent+_report.properties.practice_2_extent+_report.properties.practice_3_extent+_report.properties.practice_4_extent;
+      },
+      metricTotalAcresProtected: function(_report) {
+        return (_report.properties.total_drainage_area/43560)
+      }
     };
 
   });
--- app/modules/components/practices/modules/stormwater/stormwater--form.js
+++ app/modules/components/practices/modules/stormwater/stormwater--form.js
@@ -53,29 +53,33 @@
           'Dec'
       ];
 
-      self.stormwater_practices = [
-        'All ESD practices in MD 2007 (RR)',
-        'Bioretention or Rain Garden (Standard or Enhanced) (RR)',
-        'Dry Channel Regenerative Stormwater Conveyance (aka Step Pool Storm Conveyance) (RR)',
-        'Dry Swale (RR)',
-        'Expanded Tree Pits (RR)',
-        'Grass Channels (w/ Soil Amendments, aka Bioswale, Vegetated Swale) (RR)',
-        'Green Roof (aka Vegetated Roof) (RR)',
-        'Green Streets (RR)',
-        'Infiltration (aka Infiltration Basin, Infiltration Bed, Infiltration Trench, Dry Well/Seepage Pit, Landscape Infiltration) (RR)',
-        'Landscape Restoration/Reforestation (RR)',
-        'Non-Structural BMPs, PA 2006 BMP Manual, Chapter 5 (RR)',
-        'Permeable Pavement (aka Porous Pavement) (RR)',
-        'Rainwater Harvesting (aka Capture and Re-use) (RR)',
-        'Riparian Buffer Restoration (RR)',
-        'Rooftop Disconnection (aka Simple Disconnection to Amended Soils, to a Conservation Area, to a Pervious Area, Non-Rooftop Disconnection) (RR)',
-        'Sheetflow to Filter/Open Space* (aka Sheetflow to Conservation Area, Vegetated Filter Strip) (RR)',
-        'Constructed Wetlands (ST)',
-        'Filtering Practices (aka Constructed Filters, Sand Filters, Stormwater Filtering Systems) (ST)',
-        'Proprietary Practices (aka Manufactured BMPs) (ST)',
-        'Wet Ponds (aka Retention Basin) (ST)',
-        'Wet Swale (ST)'
-      ];
+      self.stormwater_practices = {
+        'Runoff Reduction': [
+          'All ESD practices in MD 2007',
+          'Bioretention or Rain Garden (Standard or Enhanced)',
+          'Dry Channel Regenerative Stormwater Conveyance (aka Step Pool Storm Conveyance)',
+          'Dry Swale',
+          'Expanded Tree Pits',
+          'Grass Channels (w/ Soil Amendments, aka Bioswale, Vegetated Swale)',
+          'Green Roof (aka Vegetated Roof)',
+          'Green Streets',
+          'Infiltration (aka Infiltration Basin, Infiltration Bed, Infiltration Trench, Dry Well/Seepage Pit, Landscape Infiltration)',
+          'Landscape Restoration/Reforestation',
+          'Non-Structural BMPs, PA 2006 BMP Manual, Chapter 5',
+          'Permeable Pavement (aka Porous Pavement)',
+          'Rainwater Harvesting (aka Capture and Re-use)',
+          'Riparian Buffer Restoration',
+          'Rooftop Disconnection (aka Simple Disconnection to Amended Soils, to a Conservation Area, to a Pervious Area, Non-Rooftop Disconnection)',
+          'Sheetflow to Filter/Open Space* (aka Sheetflow to Conservation Area, Vegetated Filter Strip)'
+        ],
+        'Stormwater Treatment': [
+          'Constructed Wetlands',
+          'Filtering Practices (aka Constructed Filters, Sand Filters, Stormwater Filtering Systems)',
+          'Proprietary Practices (aka Manufactured BMPs)',
+          'Wet Ponds (aka Retention Basin)',
+          'Wet Swale'
+        ]
+      };
 
       self.stormwater_project_types = [
         'New Development',
