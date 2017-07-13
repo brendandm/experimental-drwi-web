adced3f8ea26a24d256ce09020ceffdef6705404
joshuapowell <joshua@viable.io>
2017-07-13 11:15:59 -0400

    Added a new environment file

---
+++ dist/config/environment.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('environment', {name:'staging',apiUrl:'http://stg.api.fielddoc.org',siteUrl:'http://stg.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'})
+
+;
