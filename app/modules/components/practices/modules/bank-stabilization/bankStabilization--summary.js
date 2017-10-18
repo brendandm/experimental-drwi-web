00d2c51eb7cbc71b0189f4b1b9f32cf021eb102d
joshuapowell <joshua@viable.io>
2017-10-18 15:30:40 -0400

    Fixed bug with header and actions; Fixed bug with monitoring data; Fixed bug with printing; Fixed bug with notes; Fixed bug with 'Add Planning Data' showing at wrong time; Fixed bug that caused chunky loading;

--- app/modules/components/practices/modules/bank-stabilization/bankStabilization--summary.js
+++ app/modules/components/practices/modules/bank-stabilization/bankStabilization--summary.js
@@ -6,11 +6,84 @@
  * @description
  */
 angular.module('FieldDoc')
-  .controller('BankStabilizationSummaryController', function (Account, $log, $rootScope, summary, user) {
+  .controller('BankStabilizationSummaryController', function (Account, $log, PracticeBankStabilization, $rootScope, $route, $scope, summary, Utility, user, $window) {
 
     var self = this;
 
-    self.summary = summary;
+    var self = this,
+        projectId = $route.current.params.projectId,
+        siteId = $route.current.params.siteId,
+        practiceId = $route.current.params.practiceId;
+
+    $rootScope.page = {};
+
+    self.practiceType = null;
+
+    self.project = {
+      'id': projectId
+    };
+
+    self.status = {
+      loading: true
+    };
+
+    summary.$promise.then(function(successResponse) {
+
+      self.summary = successResponse;
+
+      $rootScope.page.title = self.summary.practice.properties.practice_type;
+
+      self.practiceType = Utility.machineName(self.summary.practice.properties.practice_type);
+
+      $rootScope.page.links = [
+          {
+              text: 'Projects',
+              url: '/projects'
+          },
+          {
+              text: self.summary.site.properties.project.properties.name,
+              url: '/projects/' + projectId
+          },
+          {
+            text: self.summary.site.properties.name,
+            url: '/projects/' + projectId + '/sites/' + siteId
+          },
+          {
+            text: self.summary.practice.properties.practice_type,
+            url: '/projects/' + projectId + '/sites/' + siteId + '/practices/' + practiceId,
+            type: 'active'
+          }
+      ];
+
+      $rootScope.page.actions = [
+        {
+          type: 'button-link',
+          action: function() {
+            $window.print();
+          },
+          hideIcon: true,
+          text: 'Print'
+        },
+        {
+          type: 'button-link',
+          action: function() {
+            $scope.$emit('saveToPdf');
+          },
+          hideIcon: true,
+          text: 'Save as PDF'
+        },
+        {
+          type: 'button-link new',
+          action: function() {
+            self.addReading();
+          },
+          text: 'Add Measurement Data'
+        }
+      ];
+
+      self.status.loading = false;
+      
+    }, function() {});
 
     //
     // Verify Account information for proper UI element display
@@ -27,6 +100,22 @@ angular.module('FieldDoc')
             };
         });
     }
+
+    self.addReading = function(measurementPeriod) {
+
+      var newReading = new PracticeBankStabilization({
+          'measurement_period': measurementPeriod,
+          'report_date': moment().format('YYYY-MM-DD'),
+          'practice_id': practiceId,
+          'account_id': self.site.properties.project.properties.account_id
+        });
+
+      newReading.$save().then(function(successResponse) {
+          $location.path('/projects/' + projectId + '/sites/' + siteId + '/practices/' + practiceId + '/' + self.practiceType + '/' + successResponse.id + '/edit');
+        }, function(errorResponse) {
+          console.error('ERROR: ', errorResponse);
+        });
+    };
 
 
   });
