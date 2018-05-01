23a53510dee2e43f61c316ee52489e40d0159943
Joshua Powell <joshua@viable.io>
2018-05-01 11:14:33 -0400

    Added additional nested loading to the Custom Practice "addReading" method

--- app/modules/components/practices/modules/custom/custom--form.js
+++ app/modules/components/practices/modules/custom/custom--form.js
@@ -17,6 +17,13 @@
 
       $rootScope.page = {};
 
+      self.status = {
+        loading: true,
+        reading: {
+          loading: false
+        }
+      };
+
 
       //
       // Setup Map Requirements
@@ -347,34 +354,39 @@
         self.report.$update().then(function(successResponse) {
           console.log('New reading created successfully');
 
-          // self.report = successResponse;
+          PracticeCustom.get({
+            id: $route.current.params.reportId
+          }).$promise.then(function(ssuccessResponse) {
+            self.report = ssuccessResponse;
 
-          if (self.report.properties.report_date) {
-              self.today = parseISOLike(self.report.properties.report_date);
-          }
+            if (self.report.properties.report_date) {
+                self.today = parseISOLike(self.report.properties.report_date);
+            }
 
-          //
-          // Preprocess the individual Practice Readings before display
-          //
-          if (self.report.properties.readings.length) {
+            //
+            // Preprocess the individual Practice Readings before display
+            //
+            if (self.report.properties.readings.length) {
 
-            angular.forEach(self.report.properties.readings, function(reading_, index_) {
-              self.map[reading_.id] = angular.copy(Map);
-              self.map[reading_.id] = self.buildSingleMap(reading_);
-            });
+              angular.forEach(self.report.properties.readings, function(reading_, index_) {
+                self.map[reading_.id] = angular.copy(Map);
+                self.map[reading_.id] = self.buildSingleMap(reading_);
+              });
 
-          }
+            }
 
 
-          //
-          // Check to see if there is a valid date
-          //
-          self.date = {
-              month: self.months[self.today.getMonth()],
-              date: self.today.getDate(),
-              day: self.days[self.today.getDay()],
-              year: self.today.getFullYear()
-          };
+            //
+            // Check to see if there is a valid date
+            //
+            self.date = {
+                month: self.months[self.today.getMonth()],
+                date: self.today.getDate(),
+                day: self.days[self.today.getDay()],
+                year: self.today.getFullYear()
+            };
+
+          });
 
         }, function(errorResponse) {
           console.log('New reading created successfully');
