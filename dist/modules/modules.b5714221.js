43a23399a414b95793d815d2183ef2862495535b
Aiden Powell <aidentimothy2006@gmail.com>
2018-05-15 07:53:00 -0400

    Version 3.2.1

--- dist/modules/modules.b5714221.js
+++ dist/modules/modules.b5714221.js
@@ -49,7 +49,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'staging',apiUrl:'http://stg.api.fielddoc.org',siteUrl:'http://stg.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'})
+.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1'})
 
 ;
 /**
