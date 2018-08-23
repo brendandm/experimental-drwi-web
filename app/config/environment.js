c556a5625603a490016e537f0b7afb6137451949
Lin Perez <lbp43@drexel.edu>
2018-08-23 17:03:02 -0400

    modifications to site descriptions

--- app/config/environment.js
+++ app/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS'})
+.constant('environment', {name:'development',apiUrl:'https://api.drwi.chesapeakecommons.org',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS'})
 
 ;
