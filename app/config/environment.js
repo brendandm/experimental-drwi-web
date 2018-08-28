2a7ce126094a6d2ced2697652104c901f82fabbd
Lin Perez <lbp43@drexel.edu>
2018-08-28 10:39:32 -0400

    Managing environment.js file in app/config

--- app/config/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('config', [])
-
-.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS'})
-
-;
