6db6cee7983d4592a3182ea6923dc596fcf4ea6a
brendandm <brendan.david.mcintyre@gmail.com>
2018-10-31 19:58:56 -0400

    Tidied up login and registration views

--- app/modules/shared/security/securityRegister--controller.js
+++ app/modules/shared/security/securityRegister--controller.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
     'use strict';
 
@@ -7,207 +7,204 @@
      * @name
      * @description
      */
-     angular.module('FieldDoc')
-       .controller('SecurityRegisterController', function (Account, $location, Notifications, Security, ipCookie, $rootScope, $timeout, User) {
+    angular.module('FieldDoc')
+        .controller('SecurityRegisterController',
+            function(Account, $location, Notifications, Security, ipCookie, $rootScope, $timeout, User) {
 
-             var self = this,
-                 userId = null;
+            var self = this,
+                userId = null;
 
-             self.cookieOptions = {
-               path: '/',
-               expires: 2
-             };
+            self.cookieOptions = {
+                path: '/',
+                expires: 2
+            };
 
-             //
-             // We have a continuing problem with bad data being placed in the URL,
-             // the following fixes that
-             //
-             $location.search({
-               q: undefined,
-               results_per_page: undefined
-             });
+            //
+            // We have a continuing problem with bad data being placed in the URL,
+            // the following fixes that
+            //
+            $location.search({
+                q: undefined,
+                results_per_page: undefined
+            });
 
-             self.register = {
-               data: {
-                 email: null,
-                 first_name: null,
-                 last_name: null,
-                 organizations: [],
-                 password: null
-               },
-               organizations: function() {
-                 var _organizations = [];
+            self.register = {
+                data: {
+                    email: null,
+                    first_name: null,
+                    last_name: null,
+                    password: null
+                },
+                organizations: function() {
+                    var _organizations = [];
 
-                 angular.forEach(self.register.data.organizations, function(_organization, _index) {
-                   if (_organization.id) {
-                     _organizations.push({
-                       "id": _organization.id
-                     })
-                   }
-                   else {
-                     _organizations.push({
-                       "name": _organization.properties.name
-                     })
-                   }
-                 });
+                    angular.forEach(self.register.data.organizations, function(_organization, _index) {
+                        if (_organization.id) {
+                            _organizations.push({
+                                "id": _organization.id
+                            });
+                        } else {
+                            _organizations.push({
+                                "name": _organization.properties.name
+                            });
+                        }
+                    });
 
-                 return _organizations;
-               },
-               visible: false,
-               login: function(userId) {
+                    return _organizations;
+                },
+                visible: false,
+                login: function(userId) {
 
-                 var credentials = new Security({
-                   email: self.register.data.email,
-                   password: self.register.data.password,
-                 });
+                    var credentials = new Security({
+                        email: self.register.data.email,
+                        password: self.register.data.password,
+                    });
 
-                 credentials.$save(function(response) {
+                    credentials.$save(function(response) {
 
-                   //
-                   // Check to see if there are any errors by checking for the existence
-                   // of response.response.errors
-                   //
-                   if (response.response && response.response.errors) {
+                        //
+                        // Check to see if there are any errors by checking for the existence
+                        // of response.response.errors
+                        //
+                        if (response.response && response.response.errors) {
 
-                     if (response.response.errors.email) {
-                       $rootScope.notifications.error('', response.response.errors.email);
-                     }
-                     if (response.response.errors.password) {
-                       $rootScope.notifications.error('', response.response.errors.password);
-                     }
+                            if (response.response.errors.email) {
+                                $rootScope.notifications.error('', response.response.errors.email);
+                            }
+                            if (response.response.errors.password) {
+                                $rootScope.notifications.error('', response.response.errors.password);
+                            }
 
-                     self.register.processing = false;
+                            self.register.processing = false;
 
-                     $timeout(function() {
-                       $rootScope.notifications.objects = [];
-                     }, 3500);
+                            $timeout(function() {
+                                $rootScope.notifications.objects = [];
+                            }, 3500);
 
-                     return;
-                   } else {
+                            return;
+                        } else {
 
-                     ipCookie.remove('FIELDSTACKIO_SESSION');
+                            ipCookie.remove('FIELDSTACKIO_SESSION');
 
-                     ipCookie('FIELDSTACKIO_SESSION', response.access_token, self.cookieOptions);
+                            ipCookie('FIELDSTACKIO_SESSION', response.access_token, self.cookieOptions);
 
-                     //
-                     // Make sure we also set the User ID Cookie, so we need to wait to
-                     // redirect until we're really sure the cookie is set
-                     //
-                     Account.setUserId().$promise.then(function() {
-                       Account.getUser().$promise.then(function(userResponse) {
+                            //
+                            // Make sure we also set the User ID Cookie, so we need to wait to
+                            // redirect until we're really sure the cookie is set
+                            //
+                            Account.setUserId().$promise.then(function() {
+                                Account.getUser().$promise.then(function(userResponse) {
 
-                         Account.userObject = userResponse;
+                                    Account.userObject = userResponse;
 
-                         $rootScope.user = Account.userObject;
+                                    $rootScope.user = Account.userObject;
 
-                         $rootScope.isLoggedIn = Account.hasToken();
+                                    $rootScope.isLoggedIn = Account.hasToken();
 
-                         self.newUser = new User({
-                           id: $rootScope.user.id,
-                           first_name: self.register.data.first_name,
-                           last_name: self.register.data.last_name,
-                           organizations: self.register.organizations()
-                         });
+                                    self.newUser = new User({
+                                        id: $rootScope.user.id,
+                                        first_name: self.register.data.first_name,
+                                        last_name: self.register.data.last_name
+                                    });
 
-                         self.newUser.$update().then(function (updateUserSuccessResponse) {
-                           $location.path('/projects');
-                         }, function(updateUserErrorResponse) {
-                           console.log('updateUserErrorResponse', updateUserErrorResponse);
-                         });
+                                    self.newUser.$update().then(function(updateUserSuccessResponse) {
+                                        $location.path('/account');
+                                    }, function(updateUserErrorResponse) {
+                                        console.log('updateUserErrorResponse', updateUserErrorResponse);
+                                    });
 
-                       });
-                     });
+                                });
+                            });
 
-                   }
-                 }, function(response){
+                        }
+                    }, function(response) {
 
-                   if (response.response.errors.email) {
-                     $rootScope.notifications.error('', response.response.errors.email);
-                   }
-                   if (response.response.errors.password) {
-                     $rootScope.notifications.error('', response.response.errors.password);
-                   }
+                        if (response.response.errors.email) {
+                            $rootScope.notifications.error('', response.response.errors.email);
+                        }
+                        if (response.response.errors.password) {
+                            $rootScope.notifications.error('', response.response.errors.password);
+                        }
 
-                   self.register.processing = false;
+                        self.register.processing = false;
 
-                   $timeout(function() {
-                     $rootScope.notifications.objects = [];
-                   }, 3500);
+                        $timeout(function() {
+                            $rootScope.notifications.objects = [];
+                        }, 3500);
 
-                   return;
-                 });
+                        return;
+                    });
 
-               },
-               submit: function() {
+                },
+                submit: function() {
 
-                 self.register.processing = true;
+                    self.register.processing = true;
 
-                 //
-                 // Check to see if Username and Password field are valid
-                 //
-                 if (!self.register.data.email) {
-                   $rootScope.notifications.warning('Email', 'field is required');
+                    //
+                    // Check to see if Username and Password field are valid
+                    //
+                    if (!self.register.data.email) {
+                        $rootScope.notifications.warning('Email', 'field is required');
 
-                   self.register.processing = false;
+                        self.register.processing = false;
 
-                   $timeout(function() {
-                     $rootScope.notifications.objects = [];
-                   }, 3500);
+                        $timeout(function() {
+                            $rootScope.notifications.objects = [];
+                        }, 3500);
 
-                   return;
-                 }
-                 else if (!self.register.data.password) {
-                   $rootScope.notifications.warning('Password', 'field is required');
+                        return;
+                    } else if (!self.register.data.password) {
+                        $rootScope.notifications.warning('Password', 'field is required');
 
-                   self.register.processing = false;
+                        self.register.processing = false;
 
-                   $timeout(function() {
-                     $rootScope.notifications.objects = [];
-                   }, 3500);
+                        $timeout(function() {
+                            $rootScope.notifications.objects = [];
+                        }, 3500);
 
-                   return;
-                 }
+                        return;
+                    }
 
-                 //
-                 // If all fields have values move on to the next step
-                 //
-                 Security.register({
-                   email: self.register.data.email,
-                   password: self.register.data.password
-                 }, function(response) {
+                    //
+                    // If all fields have values move on to the next step
+                    //
+                    Security.register({
+                        email: self.register.data.email,
+                        password: self.register.data.password
+                    }, function(response) {
 
-                   //
-                   // Check to see if there are any errors by checking for the
-                   // existence of response.response.errors
-                   //
-                   if (response.response && response.response.errors) {
+                        //
+                        // Check to see if there are any errors by checking for the
+                        // existence of response.response.errors
+                        //
+                        if (response.response && response.response.errors) {
 
-                     if (response.response.errors.email) {
-                       $rootScope.notifications.error('', response.response.errors.email);
-                     }
-                     if (response.response.errors.password) {
-                       $rootScope.notifications.error('', response.response.errors.password);
-                     }
+                            if (response.response.errors.email) {
+                                $rootScope.notifications.error('', response.response.errors.email);
+                            }
+                            if (response.response.errors.password) {
+                                $rootScope.notifications.error('', response.response.errors.password);
+                            }
 
-                     self.register.processing = false;
+                            self.register.processing = false;
 
-                     $timeout(function() {
-                       $rootScope.notifications.objects = [];
-                     }, 3500);
+                            $timeout(function() {
+                                $rootScope.notifications.objects = [];
+                            }, 3500);
 
-                     return;
-                   } else {
+                            return;
+                        } else {
 
-                     self.register.processing = false;
+                            self.register.processing = false;
 
-                     self.register.processingLogin = true;
+                            self.register.processingLogin = true;
 
-                     self.register.login(response.response.user.id);
-                   }
-                 });
-               }
-             };
+                            self.register.login(response.response.user.id);
+                        }
+                    });
+                }
+            };
 
-       });
+        });
 
 }());
