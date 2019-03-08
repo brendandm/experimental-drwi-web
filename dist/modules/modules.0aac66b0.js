1a1b2939281cbc35b2a4bcee75f5f403dab93046
brendandm <brendan.david.mcintyre@gmail.com>
2019-03-08 14:23:00 -0500

    Corrected bound data property in dashboard view

--- dist/modules/modules.0aac66b0.js
+++ dist/modules/modules.0aac66b0.js
@@ -83,7 +83,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'staging',apiUrl:'https://api.drwi.chesapeakecommons.org',castUrl:'https://cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://drwi.chesapeakecommons.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1551864525666})
+.constant('environment', {name:'staging',apiUrl:'https://api.drwi.chesapeakecommons.org',castUrl:'https://cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://drwi.chesapeakecommons.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1552072851210})
 
 ;
 /**
