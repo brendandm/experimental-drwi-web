5118934fb546cadaf380aaac010362685d084ddc
brendandm <brendan.david.mcintyre@gmail.com>
2019-03-12 13:19:57 -0400

    Set transition banner font weight to `normal`

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('environment', {name:'staging',apiUrl:'https://api.drwi.chesapeakecommons.org',castUrl:'https://cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://drwi.chesapeakecommons.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1552410146242})
+.constant('environment', {name:'staging',apiUrl:'https://api.drwi.chesapeakecommons.org',castUrl:'https://cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://drwi.chesapeakecommons.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1552411054747})
 
 ;
