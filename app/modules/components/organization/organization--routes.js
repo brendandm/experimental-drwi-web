a3635308920f767c75c80da95e935158cec3d3a5
rzthrun <rzthrun@gmail.com>
2019-05-28 15:45:56 -0400

    organization profile route with id created

--- app/modules/components/organization/organization--routes.js
+++ app/modules/components/organization/organization--routes.js
@@ -31,6 +31,17 @@
                             return Account.getUser();
                         }
                     }
+                }),
+             $routeProvider
+                .when('/organizationProfile/:id', {
+                    templateUrl: '/modules/components/organization/views/organizationProfile--view.html?t=' + environment.version,
+                    controller: 'OrganizationProfileViewController',
+                    controllerAs: 'page',
+                    resolve: {
+                        user: function(Account) {
+                            return Account.getUser();
+                        }
+                    }
                 });
 
         });
