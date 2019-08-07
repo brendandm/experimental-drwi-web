a31cd29f43c451abf959f1d11e34278040f5dcab
Zach Thrun <rzthrun@gmail.com>
2019-08-07 10:24:52 -0400

    build - console logs added

--- dist/modules/modules.31f850ee.js
+++ dist/modules/modules.31f850ee.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1565121507867})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1565187707026})
 
 ;
 /**
@@ -5628,11 +5628,15 @@ angular.module('FieldDoc')
                     id: organizationId
                 }).$promise.then(function(successResponse) {
 
-                    console.log('self.organizationProjects', successResponse);
+                    console.log('self.loadOrganizationProjects - >', successResponse);
 
                     self.organizationProjects = successResponse.features;
 
+                    console.log('self.organizationProjects - >', self.organizationProjects);
+
                     self.projects = successResponse.features;
+
+                    console.log('self.projects', self.projects);
 
                     self.projectCount = successResponse.count;
 
