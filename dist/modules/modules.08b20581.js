81e955ab99a96eb635d2ce0bfc95db2d58a800fe
Zach Thrun <rzthrun@gmail.com>
2019-09-25 14:53:26 -0400

    dev build

--- dist/modules/modules.08b20581.js
+++ dist/modules/modules.08b20581.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',castUrl:'http://127.0.0.1:4000',dnrUrl:'http://127.0.0.1:4000',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS',version:1569437282815})
+.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',castUrl:'http://127.0.0.1:4000',dnrUrl:'http://127.0.0.1:4000',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS',version:1569437551077})
 
 ;
 /**
