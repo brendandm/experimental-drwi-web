60f0cb976c337bd05a886955cf029c1af9ee383c
Zach Thrun <rzthrun@gmail.com>
2019-10-17 14:53:41 -0400

    dev build

--- dist/modules/modules.0a2787fd.js
+++ dist/modules/modules.0a2787fd.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571338286437})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571338401981})
 
 ;
 /**
@@ -4598,7 +4598,7 @@ angular.module('FieldDoc')
 
                     self.tags = [];
                     successResponse.features.forEach(function(feature){
-                           feature.forEach(function(f){
+                           feature.tags.forEach(function(f){
                                 self.tags.push(f);
                            });
                     });
