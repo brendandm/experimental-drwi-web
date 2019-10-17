d9c3a1926964a6039630a38701c42b34253e19d6
Zach Thrun <rzthrun@gmail.com>
2019-10-17 14:41:07 -0400

    dev build

--- dist/modules/modules.dea04b18.js
+++ dist/modules/modules.dea04b18.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571335848421})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571337515102})
 
 ;
 /**
@@ -4590,7 +4590,9 @@ angular.module('FieldDoc')
 
             self.loadTags = function() {
 
-                Tag.collection({}).$promise.then(function(successResponse) {
+                Tag.collection({
+                    group: true
+                }).$promise.then(function(successResponse) {
 
                     console.log('successResponse', successResponse);
 
