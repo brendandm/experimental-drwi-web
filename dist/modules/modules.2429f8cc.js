5d763f46e5fc6fd2fe4b6e491c390d9e60fd27c9
Zach Thrun <rzthrun@gmail.com>
2019-10-18 15:00:00 -0400

    dev build

--- dist/modules/modules.2429f8cc.js
+++ dist/modules/modules.2429f8cc.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571338546293})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571425174189})
 
 ;
 /**
@@ -4598,7 +4598,9 @@ angular.module('FieldDoc')
 
                     self.tags = [];
                     successResponse.features.forEach(function(feature){
-                          
+                           feature.tags.forEach(function(f){
+                                self.tags.push(f);
+                           });
                     });
 
                    // self.tags = successResponse.features;
@@ -32552,7 +32554,11 @@ angular.module('FieldDoc')
 
                         successResponse.features.forEach(function(feature) {
 
-                            practiceTypes.push(feature);
+                            if(feature.model_practice_type.model_id == self.model.id){
+
+                                practiceTypes.push(feature.practice_type);
+
+                            }
 
                         });
 
