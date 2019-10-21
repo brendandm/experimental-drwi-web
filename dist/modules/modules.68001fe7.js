26c17035760184f7e400c0bd1d9b8a2766a48a9e
Zach Thrun <rzthrun@gmail.com>
2019-10-21 11:06:03 -0400

    production build

--- dist/modules/modules.68001fe7.js
+++ dist/modules/modules.68001fe7.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571669358946})
+.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1571670314643})
 
 ;
 /**
@@ -4590,20 +4590,11 @@ angular.module('FieldDoc')
 
             self.loadTags = function() {
 
-                Tag.collection({
-                    group: true
-                }).$promise.then(function(successResponse) {
+                Tag.collection({}).$promise.then(function(successResponse) {
 
                     console.log('successResponse', successResponse);
 
-                    self.tags = [];
-                    successResponse.features.forEach(function(feature){
-                           feature.tags.forEach(function(f){
-                                self.tags.push(f);
-                           });
-                    });
-
-                   // self.tags = successResponse.features;
+                    self.tags = successResponse.features;
 
                     self.tags.unshift({
                         id: 0,
@@ -4612,7 +4603,6 @@ angular.module('FieldDoc')
 
                     self.filters.tag = self.tags[0].id;
 
-                    console.log("self.tags",self.tags);
                     console.log("self.filters.tag", self.filters.tag);
 
                     self.inspectSearchParams();
