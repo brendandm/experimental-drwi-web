690b2c0c7aa5617651d67ff8ed1be09330b203bc
Zach Thrun <rzthrun@gmail.com>
2019-10-21 10:19:40 -0400

    dev build

--- dist/modules/modules.5d3abd0b.js
+++ dist/modules/modules.5d3abd0b.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571666742009})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571667518993})
 
 ;
 /**
@@ -35611,16 +35611,15 @@ angular.module('FieldDoc')
                 //     isArray: false,
                 //     url: environment.apiUrl.concat('/v1/data/dashboard/:id')
                 // },
-//                collection: {
-//                    method: 'GET',
-//                    isArray: false,
-//                    url: environment.apiUrl.concat('/v1/dashboard')
-//                },
-//                basic: {
-//                    method: 'GET',
-//                    isArray: false,
-//                    url: environment.apiUrl.concat('/v1/dashboard/:id')
-//                },
+                collection: {
+                    method: 'GET',
+                    isArray: false,
+                    url: environment.apiUrl.concat('/v1/dashboard')
+                },
+                basic: {
+                    method: 'GET',/                    isArray: false,
+                    url: environment.apiUrl.concat('/v1/dashboard/:id')
+                },
                 filters: {
                     method: 'GET',
                     isArray: false,
