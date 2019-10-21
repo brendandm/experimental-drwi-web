cacddf8890d62a2adaa2ed35f0fb110f87109c15
Zach Thrun <rzthrun@gmail.com>
2019-10-21 10:06:29 -0400

    dev build

--- dist/modules/modules.338f9e58.js
+++ dist/modules/modules.338f9e58.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571425174189})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1571666742009})
 
 ;
 /**
@@ -7725,7 +7725,7 @@ angular.module('FieldDoc')
             self.createPartnership = function() {
 
                 var params = {
-                    name: self.partnerQuery.name,
+                  //  name: self.partnerQuery.name,
                     amount: self.partnerQuery.amount,
                     description: self.partnerQuery.description,
                     organization_id: self.partnerQuery.id
