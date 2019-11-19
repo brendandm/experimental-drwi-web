83eb5d1397296d5910aaa3162f6f6b069e65dd9e
Zach Thrun <rzthrun@gmail.com>
2019-11-19 15:10:20 -0500

    dev build

--- dist/modules/modules.cac5f5df.js
+++ dist/modules/modules.cac5f5df.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1574094047244})
+.constant('environment', {name:'development',apiUrl:'https://api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574194128618})
 
 ;
 /**
@@ -19857,7 +19857,6 @@ angular.module('FieldDoc')
             /*
             END Custom Extent Logic
             */
-
 
             //
             // Verify Account information for proper UI element display
