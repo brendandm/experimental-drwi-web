09d33e198dda418417773ffa8bc4eb4d783e2b01
Zach Thrun <rzthrun@gmail.com>
2019-11-20 10:09:38 -0500

    dev build

--- dist/modules/modules.763099db.js
+++ dist/modules/modules.763099db.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574198302703})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574262511240})
 
 ;
 /**
@@ -19758,9 +19758,9 @@ angular.module('FieldDoc')
 
                     self.status.processing = false;
 
-                  //  setTimeout(function(){
+                    setTimeout(function(){
                          self.loadMatrix();
-                    // }, 2000);
+                     }, 2000);
 
 
                   //  self.loadPractice();
