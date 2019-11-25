383255a34e27ca709791d0c7b50c1a74848f5fdc
Zach Thrun <rzthrun@gmail.com>
2019-11-25 14:52:37 -0500

    dev build

--- dist/modules/modules.d122895b.js
+++ dist/modules/modules.d122895b.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574711236354})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574711514008})
 
 ;
 /**
@@ -8372,16 +8372,19 @@ angular.module('FieldDoc')
 
                 console.log("SAVING PROJECT");
 
+
                 self.status.processing = true;
 
                 self.scrubFeature(self.project);
 
                 self.project.partners = self.processRelations(self.tempPartners);
 
+                console.log("self.project ->>",self.project);
+
                 var i = 0;
                 self.project.programs.forEach(function(program){
 
-                    if(self.project.programs[i][centroid] != null){
+                    if(program.centroid != null){
 
                          delete self.project.programs[i][centroid];
 
