0867fa71db5250ec196538bc9e99ef2e14721946
Zach Thrun <rzthrun@gmail.com>
2019-11-27 10:04:11 -0500

    dev build

--- dist/modules/modules.96942da7.js
+++ dist/modules/modules.96942da7.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574866679226})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1574867032387})
 
 ;
 /**
@@ -4338,7 +4338,7 @@ angular.module('FieldDoc')
                 })
              .when('/projects/:projectId/reports', {
                     templateUrl: '/modules/components/projects/views/projectsReports--view.html?t=' + environment.version,
-                    controller: 'ProjectsBatchDeleteController',
+                    controller: 'ProjectsReportsController',
                     controllerAs: 'page',
                     resolve: {
                         user: function(Account, $rootScope, $document) {
