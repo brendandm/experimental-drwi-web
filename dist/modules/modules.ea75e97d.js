ea44e72bb46a6fb00ea1215628c4bb9a69ef13b4
Zach Thrun <rzthrun@gmail.com>
2019-12-04 12:15:45 -0500

    dev build

--- dist/modules/modules.ea75e97d.js
+++ dist/modules/modules.ea75e97d.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575479149427})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575479646048})
 
 ;
 /**
@@ -9627,14 +9627,14 @@ angular.module('FieldDoc')
 
                 self.loadReports = function(){
                     console.log("Loading Reports");
-              /*      report.projectBundle({}).$promise.then(function(successResponse) {
+                    report.projectBundle({}).$promise.then(function(successResponse) {
                         console.log("successResponse");
                         console.log(successResponse);
                     }, function(errorResponse){
                         console.log("errorResponse");
                         console.log(errorResponse);
                     });
-                    */
+                    
                 };
 
 
