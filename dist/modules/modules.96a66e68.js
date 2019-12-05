a5d4955a6de7e4b14154dc5030a34106eb9486c1
Zach Thrun <rzthrun@gmail.com>
2019-12-05 14:30:41 -0500

    dev build

--- dist/modules/modules.96a66e68.js
+++ dist/modules/modules.96a66e68.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575573953919})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575574213683})
 
 ;
 /**
@@ -9785,7 +9785,7 @@ angular.module('FieldDoc')
 
                             $rootScope.page.title = 'Project Batch Delete';
 
-                         //   self.loadSites();
+                            self.loadReport();
 
                         }
 
