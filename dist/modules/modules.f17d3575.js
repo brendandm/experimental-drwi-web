a2ff3426902e014b1474103b8d5ae26fc689c1d4
Zach Thrun <rzthrun@gmail.com>
2019-12-05 14:57:47 -0500

    dev build

--- dist/modules/modules.f17d3575.js
+++ dist/modules/modules.f17d3575.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575574522877})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575575839379})
 
 ;
 /**
@@ -9720,6 +9720,8 @@ angular.module('FieldDoc')
 
                 self.params;
 
+                self.reportId = +$routeParams.reportId;
+
                 $rootScope.toolbarState = {
                     'edit': true
                 };
@@ -9805,9 +9807,13 @@ angular.module('FieldDoc')
 
                 self.loadReport = function(){
 
+                };
+
+                self.loadBundle = function(){
+
                     console.log("Loading Report");
 
-                    Report.reportBundle().$promise.then(function(successResponse) {
+                    Report.reportBundle({id:self.reportId}).$promise.then(function(successResponse) {
 
                         console.log("successResponse");
 
