b44c6a51a96a3942ee185da78598ea247f5790f7
Zach Thrun <rzthrun@gmail.com>
2019-12-05 15:35:31 -0500

    dev build

--- dist/modules/modules.1e182670.js
+++ dist/modules/modules.1e182670.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575577946621})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575578096079})
 
 ;
 /**
@@ -9632,7 +9632,7 @@ angular.module('FieldDoc')
 
                     self.reports = self.project.reports;
 
-                    self.programs = self.projects.programs;
+                    self.programs = self.project.programs;
 
                     self.matrix = [];
 
