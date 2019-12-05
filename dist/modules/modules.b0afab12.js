1cc25e80cc76b7c2af09a7bb22ae45426ee5d3e5
Zach Thrun <rzthrun@gmail.com>
2019-12-05 16:45:21 -0500

    dev build

--- dist/modules/modules.b0afab12.js
+++ dist/modules/modules.b0afab12.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575581899583})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575582300535})
 
 ;
 /**
@@ -9772,7 +9772,7 @@ angular.module('FieldDoc')
 
                 self.reportId = $route.current.params.reportId;
 
-               
+
 
 
                 $rootScope.toolbarState = {
@@ -9846,7 +9846,9 @@ angular.module('FieldDoc')
 
                         self.tags = Utility.processTags(self.project.tags);
 
-                        // self.showElements();
+                         self.showElements();
+
+                         self.loadBundle ();
 
                     }).catch(function(errorResponse) {
 
