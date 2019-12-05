a7defcc28a20111f4425abee961b871accbcb37d
Zach Thrun <rzthrun@gmail.com>
2019-12-05 15:50:06 -0500

    dev build

--- dist/modules/modules.b089f8d4.js
+++ dist/modules/modules.b089f8d4.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575578489256})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575578920972})
 
 ;
 /**
@@ -9646,9 +9646,9 @@ angular.module('FieldDoc')
 
                         self.reports.forEach(function(report,r_i){
 
-                            if(program.id == report.id){
+                            if(program.id == report.program_id){
 
-                                self.matrix.push(report);
+                                self.matrix[i].reports.push(report);
 
                             }
 
