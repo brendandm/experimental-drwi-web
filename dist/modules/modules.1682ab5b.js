0c353879af0f8fa35bdaf9b5205938e2d374630b
Zach Thrun <rzthrun@gmail.com>
2019-12-08 19:57:05 -0500

    dev build

--- dist/modules/modules.1682ab5b.js
+++ dist/modules/modules.1682ab5b.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575849687947})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575852990586})
 
 ;
 /**
@@ -9693,7 +9693,21 @@ angular.module('FieldDoc')
                 self.addReport = function(program_id){
 
                     console.log("Add Report to program id# "+program_id);
-    
+
+                    Report.reportBundle({
+                         program_id: program_id
+                        })
+                    .$promise.then(function(successResponse) {
+
+                        console.log(successResponse);
+
+                    }, function(errorResponse){
+
+                        console.log(errorResponse);
+
+                    });
+
+
                 };
 
               
