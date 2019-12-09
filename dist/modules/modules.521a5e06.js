4e5d65de4c56bf94da1d61b72e2b461c0ee933e7
Zach Thrun <rzthrun@gmail.com>
2019-12-09 12:50:06 -0500

    dev build

--- dist/modules/modules.521a5e06.js
+++ dist/modules/modules.521a5e06.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575913632344})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575913785820})
 
 ;
 /**
@@ -35379,7 +35379,7 @@ angular
                 createReportBundle:{
                     method: 'POST',
                     isArray: false,
-                    url: environment.apiUrl.concat('/v1/report-bundle/')
+                    url: environment.apiUrl.concat('/v1/report-bundle/:id')
                 },
 
                 projectReport:{
