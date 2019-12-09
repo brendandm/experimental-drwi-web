317de8e35105786abeb1124982c7cfa926db4963
Zach Thrun <rzthrun@gmail.com>
2019-12-09 12:11:57 -0500

    dev build

--- dist/modules/modules.8f5037ea.js
+++ dist/modules/modules.8f5037ea.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575911250178})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575911390991})
 
 ;
 /**
@@ -9699,7 +9699,7 @@ angular.module('FieldDoc')
                             "title": "I eat food",
                             "notes": "Cats are super",
                             "date": "2019-12-09",
-                            "organization_id": self.project.organization_id,
+                            "organization_id": 190,
                             "project_id": 2671,
                             "program_id": 4
                         }
