576ae162ea4b79193ca177047bbb444d3c8c95d6
Zach Thrun <rzthrun@gmail.com>
2019-12-10 09:53:44 -0500

    dev build

--- dist/modules/modules.db5d4f35.js
+++ dist/modules/modules.db5d4f35.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575989420908})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1575989562641})
 
 ;
 /**
@@ -9714,7 +9714,7 @@ angular.module('FieldDoc')
 
                         self.newReport = successResponse;
 
-                        $location.path('/'+self.newReport.id);
+                        $location.path('/projects/'+self.project.id"/reports/"+self.newReport.id);
 
                     }, function(errorResponse){
 
