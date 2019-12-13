0f2bffdf7631acc6b7ff9b87531e06f9cdc9b005
Zach Thrun <rzthrun@gmail.com>
2019-12-13 12:59:58 -0500

    dev build

--- dist/modules/modules.c9a23de8.js
+++ dist/modules/modules.c9a23de8.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576259323815})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576259962581})
 
 ;
 /**
@@ -9953,7 +9953,7 @@ angular.module('FieldDoc')
 
                     arr.forEach(function(feature, index) {
 
-                         if (feature.properties.project.extent) {
+                         if (feature.project.extent) {
 
                             if(feature.geometry != null){
 
