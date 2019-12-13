76ea9651d19fcdcc25cc103b6e26ceedcf5e2515
Zach Thrun <rzthrun@gmail.com>
2019-12-13 12:52:24 -0500

    dev build

--- dist/modules/modules.e6df05a1.js
+++ dist/modules/modules.e6df05a1.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576259259118})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576259323815})
 
 ;
 /**
@@ -9959,7 +9959,7 @@ angular.module('FieldDoc')
 
                                 feature.staticURL = Utility.buildStaticMapURL(feature.geometry);
 
-                                self.sites[index].staticURL = feature.staticURL;
+                                self.practices[index].staticURL = feature.staticURL;
 
                             }else{
 
