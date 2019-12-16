7f07d716ba3e8cf05f703afb4d57573a3e92e7e9
Zach Thrun <rzthrun@gmail.com>
2019-12-16 01:08:29 -0500

    dev build

--- dist/modules/modules.a1d3cd54.js
+++ dist/modules/modules.a1d3cd54.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576476289102})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576476469919})
 
 ;
 /**
@@ -9984,7 +9984,7 @@ angular.module('FieldDoc')
 
                                  console.log("FIVE");
 
-                                self.sites[index].staticURL = ['https://api.mapbox.com/styles/v1',
+                                self.practices[index].staticURL = ['https://api.mapbox.com/styles/v1',
                                                             '/mapbox/streets-v11/static/0,0,3,0/400x200?access_token=',
                                                             'pk.eyJ1IjoiYm1jaW50eXJlIiwiYSI6IjdST3dWNVEifQ.ACCd6caINa_d4EdEZB_dJw'
                                                         ].join('');
