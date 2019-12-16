d3ec7e72dc9672f634ba00326acc6298b54965b9
Zach Thrun <rzthrun@gmail.com>
2019-12-16 00:54:19 -0500

    dev build

--- dist/modules/modules.135d2568.js
+++ dist/modules/modules.135d2568.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576475443698})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576475633569})
 
 ;
 /**
@@ -9845,6 +9845,9 @@ angular.module('FieldDoc')
                         $timeout(function() {
 
                             console.log("YES A");
+
+                            console.log("self.practices",self.practices);
+                            console.log("self.practices.length",self.practices.length);
 
                             if (self.practices && self.practices.length) {
 
