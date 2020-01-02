de24a057a554811a27b05d367c237164e0de381f
Zach Thrun <rzthrun@gmail.com>
2020-01-02 13:58:57 -0500

    dev build

--- dist/modules/modules.8c4081fc.js
+++ dist/modules/modules.8c4081fc.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1576818746716})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1577991502566})
 
 ;
 /**
@@ -10065,7 +10065,20 @@ angular.module('FieldDoc')
 
                     console.log("SHOW PRACTICE "+p_id);
 
+                    self.displayModal = true;
+
                 };
+
+                 self.closePracticeModal = function(){
+
+                    console.log("CLOSE PRACTICE ");
+
+                    self.displayModal = false;
+
+                };
+
+
+
 
 
                 self.loadPractices = function(){
