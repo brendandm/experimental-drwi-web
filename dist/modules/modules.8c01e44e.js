165f13e5dd747aeeece193f0f6e8e6b7bfac1bf6
Zach Thrun <rzthrun@gmail.com>
2020-01-05 17:43:53 -0500

    dev build

--- dist/modules/modules.8c01e44e.js
+++ dist/modules/modules.8c01e44e.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578264023172})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578264211451})
 
 ;
 /**
@@ -10065,7 +10065,7 @@ angular.module('FieldDoc')
 
                     console.log("SHOW PRACTICE "+p_id);
 
-                    var self.selectedPractice;
+                    self.selectedPractice = {};
 
                     self.practices.forEach(practice, function(){
 
