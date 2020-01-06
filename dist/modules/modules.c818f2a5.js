900c2e9b91254e9abffbaa018bc9cae7dc7923c5
Zach Thrun <rzthrun@gmail.com>
2020-01-06 15:04:43 -0500

    dev build ++

--- dist/modules/modules.c818f2a5.js
+++ dist/modules/modules.c818f2a5.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578335077398})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578341053826})
 
 ;
 /**
@@ -10104,6 +10104,8 @@ angular.module('FieldDoc')
                             console.log("self.selectedPractice.report", self.selectedPractice.report);
 
                             console.log(" self.selectedPractice", self.selectedPractice);
+
+                            console.log("self.selectedPractice.report.id",self.selectedPractice.report.id);
 
                             if(self.selectedPractice.report.id > 0){
                                 console.log("SHOW EXISTING REPORT:",self.selectedPractice.report);
