b02993e94efdb88f70218d50b3e6260db971fa8e
Zach Thrun <rzthrun@gmail.com>
2020-01-06 11:20:40 -0500

    dev build

--- dist/modules/modules.5ec7655c.js
+++ dist/modules/modules.5ec7655c.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578327162312})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578327584326})
 
 ;
 /**
@@ -10033,10 +10033,11 @@ angular.module('FieldDoc')
                  self.addReading = function(measurementPeriod) {
 
                     var newReading = new Report({
-                        'measurement_period': 'Installation',
-                        'report_date': new Date(),
-                        'practice_id': self.selectedPractice.id,
-                        'organization_id': self.selectedPractice.organization_id
+                        'measurement_period'    : 'Installation',
+                        'report_date'           : new Date(),
+                        'practice_id'           : self.selectedPractice.id,
+                        'organization_id'       : self.selectedPractice.organization_id,
+                        'bundle_id'             : self.bundle.id
                     });
 
                     newReading.$save().then(function(successResponse) {
