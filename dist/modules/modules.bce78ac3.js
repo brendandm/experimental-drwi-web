0344223bd2a763bbe1b60b2fa7cf4c0ba0792d25
Zach Thrun <rzthrun@gmail.com>
2020-01-13 10:03:31 -0500

    dev build

--- dist/modules/modules.bce78ac3.js
+++ dist/modules/modules.bce78ac3.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578926140835})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578927743347})
 
 ;
 /**
@@ -20687,9 +20687,7 @@ angular.module('FieldDoc')
             });
 
 }());
-/*
-init commit
-*/
+
 'use strict';
 
 /**
