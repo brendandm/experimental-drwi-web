a509ed0059730af9061607d220068f2a8d0842ad
Zach Thrun <rzthrun@gmail.com>
2020-01-12 21:24:00 -0500

    syntax error fix

--- dist/modules/modules.9b806546.js
+++ dist/modules/modules.9b806546.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578882050490})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1578882216457})
 
 ;
 /**
@@ -9702,7 +9702,7 @@ angular.module('FieldDoc')
 
                     // Here a date has been assigned
                     // while creating Date object
-                    var dateobj = new Date.now;
+                    var dateobj = new Date.now();
 
                     // Contents of above date object is converted
                     // into a string using toISOString() function.
