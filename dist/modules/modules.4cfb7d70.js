615b5f02c8c5a1633edf157786688c8cc96f5f4d
Zach Thrun <rzthrun@gmail.com>
2020-01-15 12:12:18 -0500

    scope correction

--- dist/modules/modules.4cfb7d70.js
+++ dist/modules/modules.4cfb7d70.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1579108164283})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1579108314132})
 
 ;
 /**
@@ -10528,13 +10528,11 @@ angular.module('FieldDoc')
 
                         });
 
-                    };
+                    }
 
 
 
 
-
-                }
 
 
 
