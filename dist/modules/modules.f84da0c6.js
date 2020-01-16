3740cc5739e9879f4bd39b52223fb75a234e72d3
Zach Thrun <rzthrun@gmail.com>
2020-01-16 09:43:11 -0500

    bundle save and bundle date update

--- dist/modules/modules.f84da0c6.js
+++ dist/modules/modules.f84da0c6.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1579108314132})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1579185770423})
 
 ;
 /**
@@ -9935,7 +9935,7 @@ angular.module('FieldDoc')
 
                             var _new = response.year + '-' + response.month.numeric + '-' + response.date,
                                 _date = new Date(_new);
-                            self.date.day = self.days[_date.getDay()];
+                            self.bundle.date.day = self.days[_date.getDay()];
 
                         }
 
@@ -9975,7 +9975,7 @@ angular.module('FieldDoc')
                             //
                             // Check to see if there is a valid date
                             //
-                            self.date = {
+                            self.bundle.date = {
                                 month: self.months[self.today.getMonth()],
                                 date: self.today.getDate(),
                                 day: self.days[self.today.getDay()],
@@ -10411,8 +10411,8 @@ angular.module('FieldDoc')
 
                  //   self.scrubFeature(self.report);
 
-                    if (self.date.month.numeric !== null &&
-                        typeof self.date.month.numeric === 'string') {
+                    if (self.report.report_date.month.numeric !== null &&
+                        typeof self.report.report_date.month.numeric === 'string') {
 
                         self.report.report_date = self.reportDate.year + '-' + self.reportDate.month.numeric + '-' + self.reportDate.date;
 
@@ -10490,10 +10490,11 @@ angular.module('FieldDoc')
 
                     console.log("SAVE THAT BUNDLE !!!");
 
+                    console.log(self.bundle);
 
                     Report.updateBundle({
                         id: self.bundle.id
-                        }, data).$promise.then(function(successResponse) {
+                        }, self.bundle).$promise.then(function(successResponse) {
 
                             self.alerts = [{
                                 'type': 'success',
