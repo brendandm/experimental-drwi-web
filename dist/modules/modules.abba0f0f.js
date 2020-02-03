02e1c0188fa1e1ab5b643dcae73f13208acbeee2
Zach Thrun <rzthrun@gmail.com>
2020-02-03 10:11:00 -0500

    programPracticeList added to program

--- dist/modules/modules.abba0f0f.js
+++ dist/modules/modules.abba0f0f.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1580742217020})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1580742581604})
 
 ;
 /**
@@ -29468,6 +29468,164 @@ angular.module('FieldDoc')
             }
 
         });
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc function
+     * @name
+     * @description
+     */
+    angular.module('FieldDoc')
+        .controller('ModelSummaryController', [
+            'Account',
+            '$location',
+            '$timeout',
+            '$log',
+            '$rootScope',
+            '$route',
+            'Utility',
+            'user',
+            '$window',
+            'mapbox',
+            'Model',
+            'Project',
+            'model',
+            'Program'
+            function(Account, $location, $timeout, $log, $rootScope,
+                $route, Utility, user, $window, mapbox, Model,
+                Project, model) {
+
+                var self = this;
+
+                self.modelId = $route.current.params.modelId;
+
+                $rootScope.viewState = {
+                    'model': true
+                };
+
+                $rootScope.toolbarState = {
+                    'dashboard': true
+                };
+
+                $rootScope.page = {};
+
+                self.status = {
+                    loading: true
+                };
+
+                self.alerts = [];
+
+                function closeAlerts() {
+
+                    self.alerts = [];
+
+                }
+
+                function closeRoute() {
+
+                    $location.path(self.model.links.site.html);
+
+                }
+
+                self.loadModel = function() {
+
+                    model.$promise.then(function(successResponse) {
+
+                        console.log('self.model', successResponse);
+
+                        self.model = successResponse;
+
+                        $rootScope.model = successResponse;
+
+                        $rootScope.page.title = self.model.name ? self.model.name : 'Un-named Model';
+
+                        self.status.loading = false;
+
+                        self.loadPractices();
+
+                    }, function(errorResponse) {
+
+
+
+                    });
+
+                };
+
+                self.loadTags = function() {
+
+                    //
+
+                };
+
+                self.loadMetrics = function() {
+
+                    //
+
+                };
+
+                self.loadProjects = function() {
+
+                    //
+
+                };
+
+                self.loadPractices = function() {
+
+                    Model.practiceTypes({
+                        id: self.model.id
+                    }).$promise.then(function(successResponse) {
+
+                        console.log('Model.practiceTypes successResponse', successResponse);
+
+                        var practiceTypes = [];
+
+                        successResponse.features.forEach(function(feature) {
+
+                            if(feature.model_practice_type.model_id == self.model.id){
+
+                                practiceTypes.push(feature.practice_type);
+
+                            }
+
+                        });
+
+                        self.practiceTypes = practiceTypes;
+
+                    }, function(errorResponse) {
+
+                        console.log('Model.practiceTypes errorResponse', errorResponse);
+
+                    });
+
+                };
+
+                //
+                // Verify Account information for proper UI element display
+                //
+                if (Account.userObject && user) {
+
+                    user.$promise.then(function(userResponse) {
+
+                        $rootScope.user = Account.userObject = userResponse;
+
+                        self.permissions = {
+                            isLoggedIn: Account.hasToken(),
+                            role: $rootScope.user.properties.roles[0],
+                            account: ($rootScope.account && $rootScope.account.length) ? $rootScope.account[0] : null,
+                            can_edit: true
+                        };
+
+                        self.loadModel();
+
+                    });
+                }
+
+            }
+        ]);
+
+}());
 'use strict';
 
 /**
