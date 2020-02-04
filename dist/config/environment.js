eeb29d5364e76dbc8912b20f0a13f1872c70180d
Zach Thrun <rzthrun@gmail.com>
2020-02-04 15:35:57 -0500

    production build

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1580848423000})
+.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1580848540597})
 
 ;
