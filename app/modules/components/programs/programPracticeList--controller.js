fd90f053ad61841405ba41adb75a6c51af0f2cc7
Zach Thrun <rzthrun@gmail.com>
2020-02-04 12:55:28 -0500

    controller updated

--- app/modules/components/programs/programPracticeList--controller.js
+++ app/modules/components/programs/programPracticeList--controller.js
@@ -8,7 +8,7 @@
      * @description
      */
     angular.module('FieldDoc')
-        .controller('ProgramSummaryController', [
+        .controller('ProgramPracticeListController', [
             'Account',
             '$location',
             '$timeout',
