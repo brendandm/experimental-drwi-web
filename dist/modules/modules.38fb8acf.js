1d7c01b9ece52751ee77fdb3f44ddc986b67b385
Zach Thrun <rzthrun@gmail.com>
2020-02-07 00:58:08 -0500

    correction

--- dist/modules/modules.38fb8acf.js
+++ dist/modules/modules.38fb8acf.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1581054959917})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1581055065229})
 
 ;
 /**
@@ -4414,7 +4414,7 @@ angular.module('FieldDoc')
                if(self.page > 1){
                     self.viewCountLow = (self.page * self.limit);
                     if(self.summary.feature_count > ((self.page * self.limit) + self.limit)){
-                        self.viewCountHigh = ((self.page * self.limit) + self.limit));
+                        self.viewCountHigh = ((self.page * self.limit) + self.limit);
                     }else{
                         self.viewCountHigh = self.summary.feature_count;
                     }
