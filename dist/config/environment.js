ebe2c1ba530b46730b40abd85551436bf4f137b8
Zach Thrun <rzthrun@gmail.com>
2020-03-02 13:34:02 -0500

    rollback to master prior to FD_392

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1582137110860})
+.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1583174002889})
 
 ;
