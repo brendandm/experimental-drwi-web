de4c4abdc3e7f5d8807f3afb1f098897d4622604
Zach Thrun <rzthrun@gmail.com>
2020-03-04 12:24:54 -0500

    typo correction

--- dist/modules/modules.f8ad583f.js
+++ dist/modules/modules.f8ad583f.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1583340839496})
+.constant('environment', {name:'production',apiUrl:'https://api.fielddoc.org',siteUrl:'https://www.fielddoc.org',clientId:'lynCelX7eoAV1i7pcltLRcNXHvUDOML405kXYeJ1',version:1583342606621})
 
 ;
 /**
