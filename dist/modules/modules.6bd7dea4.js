4c180a3a1316eaf176d0517050e4987e62d9c9c4
Zach Thrun <rzthrun@gmail.com>
2020-05-16 18:12:43 -0400

    console.log

--- dist/modules/modules.6bd7dea4.js
+++ dist/modules/modules.6bd7dea4.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1589665347333})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1589667141480})
 
 ;
 /**
@@ -4852,6 +4852,9 @@ angular.module('FieldDoc')
             // Verify Account information for proper UI element display
             //
             if (Account.userObject && user) {
+
+                console.log("Account.userObject",Account.userObject);
+                console.log("user",user);
 
                 user.$promise.then(function(userResponse) {
 
