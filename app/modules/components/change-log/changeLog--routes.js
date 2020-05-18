281d820626afdc1daf0bf1eb7450a3a6806dcba3
Zach Thrun <rzthrun@gmail.com>
2020-05-18 14:22:36 -0400

    init commit -- changeLog module added

---
+++ app/modules/components/change-log/changeLog--routes.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name FieldDoc
+ * @description
+ * # FieldDoc
+ *
+ * Main module of the application.
+ */
+angular.module('FieldDoc')
+    .config(function($routeProvider, environment) {
+
+        $routeProvider
+            .when('/:feature_type/:feature_id/changeLog/', {
+                         templateUrl: '/modules/components/change-log/views/changeLog--view.html?t=' + environment.version,
+                controller: 'ChangeLogController',
+                controllerAs: 'page',
+                reloadOnSearch: false,
+                resolve: {
+                /*    projects: function($location, Project, $rootScope) {
+
+                        return Project.collection({});
+
+                    },
+                    */
+                    user: function(Account, $rootScope, $document) {
+
+                        $rootScope.targetPath = document.location.pathname;
+
+                        if (Account.userObject && !Account.userObject.id) {
+                            return Account.getUser();
+                        }
+
+                        return Account.userObject;
+
+                    }
+                }
+            });
+
+    });
