cef4e10c4fb4d50c00902f4bcb4ffbce25074a8a
Zach Thrun <rzthrun@gmail.com>
2020-05-20 13:54:27 -0400

    correction

--- dist/modules/modules.cf48a2e1.js
+++ dist/modules/modules.cf48a2e1.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1589996882515})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1589997245626})
 
 ;
 /**
@@ -11098,7 +11098,7 @@ angular.module('FieldDoc')
                           var c = 0;
                           group.changes.forEach(function(change){
                              if(change.diff.hasOwnProperty('geometry')){
-                                self.changeLog[i].changes[c].diff.geometry.new_staticURL =Utility.buildStaticMapURL(change.diff.geometry.new_value.coordinates,self.feature_type);;
+                                self.changeLog[i].changes[c].diff.geometry.new_staticURL =Utility.buildStaticMapURL(changes.diff.geometry.new_value.coordinates,self.feature_type);;
 
                              }
                              c = c+1;
