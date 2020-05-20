a8e1851775b6e9a18584bd05e65fd4b066eb0f18
Zach Thrun <rzthrun@gmail.com>
2020-05-20 13:59:04 -0400

    check if group.change.diff has value in resposeParser

--- dist/modules/modules.46cb5397.js
+++ dist/modules/modules.46cb5397.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1589997359996})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1589997506776})
 
 ;
 /**
@@ -11097,9 +11097,11 @@ angular.module('FieldDoc')
 
                           var c = 0;
                           group.changes.forEach(function(change){
-                             if(change.diff.hasOwnProperty('geometry')){
-                                self.changeLog[i].changes[c].diff.geometry.new_staticURL =Utility.buildStaticMapURL(change.diff.geometry.new_value.coordinates,self.feature_type);;
+                              if(group.change.diff =!= null){
+                                 if(change.diff.hasOwnProperty('geometry')){
+                                    self.changeLog[i].changes[c].diff.geometry.new_staticURL =Utility.buildStaticMapURL(change.diff.geometry.new_value.coordinates,self.feature_type);;
 
+                                 }
                              }
                              c = c+1;
                           });
