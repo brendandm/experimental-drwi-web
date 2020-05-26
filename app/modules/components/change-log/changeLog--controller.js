39fc56c3b1bbf199abd3e52bb726ab895dc040d6
Zach Thrun <rzthrun@gmail.com>
2020-05-25 21:59:55 -0400

    console log

--- app/modules/components/change-log/changeLog--controller.js
+++ app/modules/components/change-log/changeLog--controller.js
@@ -244,6 +244,7 @@ angular.module('FieldDoc')
 
                                     self.changeLog[i].changes[c].diff = {};
                                    for (var item in change.data) {
+                                        console.log("change.action",change.action);
                                         if(change.action.includes("create")){
 
                                             self.changeLog[i].changes[c].diff[item] = {new_display:{}};
