aa1178be93c7eaf992ab903edc83dc2a4b63f011
Zach Thrun <rzthrun@gmail.com>
2020-06-19 14:46:37 -0400

    build to force

--- dist/modules/modules.f44cfabe.js
+++ dist/modules/modules.f44cfabe.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1592592135931})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1592592370364})
 
 ;
 /**
@@ -22169,6 +22169,13 @@ angular.module('FieldDoc')
             self.deleteTarget = function($item,$index){
 
                 console.log("$delete $item,",$item)
+
+                var target_arr = [];
+                target_arr.push($item);
+
+                var data = {
+                    targets: target_arr
+                };
 
                 Practice.targetDelete({
                     id: +self.practice.id,
