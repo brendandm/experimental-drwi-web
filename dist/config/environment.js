465e7e762a5a2349d44e43a31b38b44500ee6778
Zach Thrun <rzthrun@gmail.com>
2020-06-29 10:31:40 -0400

    rebuild

--- dist/config/environment.js
+++ dist/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',castUrl:'http://127.0.0.1:4000',dnrUrl:'http://127.0.0.1:4000',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS',version:1593440542921})
+.constant('environment', {name:'local',apiUrl:'http://127.0.0.1:5000',castUrl:'http://127.0.0.1:4000',dnrUrl:'http://127.0.0.1:4000',siteUrl:'http://127.0.0.1:9000',clientId:'sL3yMmmnSNszktuQGVBCasZ6mCy7DahS',version:1593441080513})
 
 ;
