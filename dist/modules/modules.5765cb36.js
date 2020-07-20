85a4b28a11db417775b1fcc34ae2e797b39e1c19
Zach Thrun <rzthrun@gmail.com>
2020-07-20 10:48:22 -0400

    project summary tile add + site , practice button fix

--- dist/modules/modules.5765cb36.js
+++ dist/modules/modules.5765cb36.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1594938790906})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1595256438632})
 
 ;
 /**
@@ -39219,7 +39219,7 @@ angular.module('FieldDoc')
 
                         scope.confirmDelete = function () {
 
-                            scope.showDeletionDialog = true;
+                            scope.showDeletionDialog = !scope.showDeletionDialog;
 
                         };
 
@@ -39289,7 +39289,7 @@ angular.module('FieldDoc')
 
                         scope.confirmCopy = function() {
 
-                            scope.showCopyDialog = true;
+                            scope.showCopyDialog = !scope.showCopyDialog;
 
                         };
 
