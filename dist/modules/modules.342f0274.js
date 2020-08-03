5393ea20a06b3e65e3bc565a5e7580742ab0e7d9
Zach Thrun <rzthrun@gmail.com>
2020-08-03 11:02:50 -0400

    dev build

--- dist/modules/modules.342f0274.js
+++ dist/modules/modules.342f0274.js
@@ -125,7 +125,7 @@ angular.module('FieldDoc')
 
  angular.module('config', [])
 
-.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1596466292330})
+.constant('environment', {name:'development',apiUrl:'https://dev.api.fielddoc.org',castUrl:'https://dev.cast.fielddoc.chesapeakecommons.org',dnrUrl:'https://dev.dnr.fielddoc.chesapeakecommons.org',siteUrl:'https://dev.fielddoc.org',clientId:'2yg3Rjc7qlFCq8mXorF9ldWFM4752a5z',version:1596466946267})
 
 ;
 /**
@@ -34815,7 +34815,7 @@ angular.module('FieldDoc')
                 if (fitBounds === null ||
 
                     typeof fitBounds === 'undefined') {
-               //     console.log("B");
+
                     fitBounds = true;
 
                 }
