77702af20fa272e80d7fd48a763e2d40142f5df0
brendanmcintyre <brendan.david.mcintyre@gmail.com>
2020-10-27 09:11:36 -0400

    Expanded breadcrumb directive coverage

--- app/modules/shared/directives/breadcrumb/breadcrumb--directive.js
+++ app/modules/shared/directives/breadcrumb/breadcrumb--directive.js
@@ -5,12 +5,16 @@
     angular.module('FieldDoc')
         .directive('breadcrumb', [
             'environment',
-            function (environment) {
+            '$timeout',
+            function (environment, $timeout) {
                 return {
                     restrict: 'EA',
                     scope: {
+                        'metric': '=?',
                         'pad': '@',
                         'practice': '=?',
+                        'practiceType': '=?',
+                        'program': '=?',
                         'project': '=?',
                         'report': '=?',
                         'site': '=?',
@@ -29,31 +33,62 @@
 
                         scope.calcAvailWidth = function () {
 
-                            var parent = document.querySelector('.breadcrumb');
-                            
+                            var parent = document.querySelector(
+                                '.breadcrumb');
+
                             var parentWidth;
-                            
+
                             if (parent) {
-                                
+
                                 parentWidth = (parent.clientWidth * 0.90);
-                                
+
                             }
-                            
-                            var anchorEls = document.querySelectorAll('.anchor');
-                            
+
+                            var anchorEls = document.querySelectorAll(
+                                '.anchor');
+
                             var anchorWidth = 0;
 
                             for (var i = 0; i < anchorEls.length; i++) {
 
                                 var el = anchorEls[i];
-                                
+
                                 anchorWidth += el.clientWidth;
 
                             }
-                            
+
                             return parentWidth - anchorWidth;
 
                         };
+
+                        // scope.collapse = function (availWidth) {
+                        //
+                        //     var collapseEls = document.querySelectorAll(
+                        //         '.breadcrumb .collapse');
+                        //
+                        //     var count = collapseEls.length;
+                        //
+                        //     console.log(
+                        //         'collapse:count:',
+                        //         count
+                        //     );
+                        //
+                        //     var width = Math.floor(availWidth / count);
+                        //
+                        //     console.log(
+                        //         'collapse:width:',
+                        //         width
+                        //     );
+                        //
+                        //     for (var i = 0; i < count; i++) {
+                        //
+                        //         var el = collapseEls[i];
+                        //
+                        //         el.style.width = width + 'px';
+                        //
+                        //     }
+                        //
+                        // };
 
                         scope.setBasis = function () {
 
@@ -66,53 +101,66 @@
                             if (scope.practice && !scope.project) return;
 
                             if (scope.site && !scope.project) return;
-                            
-                            var ancestors = [
-                                scope.practice,
-                                scope.site,
-                                scope.project
-                            ];
-                            
-                            var ancestorCount = 0;
-                            
-                            ancestors.forEach(function (feature) {
-                                
-                                if (feature !== null && feature !== 'undefined') {
 
-                                    ancestorCount += 1;
-                                    
-                                }
+                            // var ancestors = [
+                            //     scope.practice,
+                            //     scope.site,
+                            //     scope.project
+                            // ];
+                            //
+                            // var ancestorCount = 0;
+                            //
+                            // ancestors.forEach(function (feature) {
+                            //
+                            //     if (feature !== null && feature !== 'undefined') {
+                            //
+                            //         ancestorCount += 1;
+                            //
+                            //     }
+                            //
+                            // });
 
-                            });
+                            $timeout(function () {
 
-                            console.log(
-                                'calcAvailWidth',
-                                scope.calcAvailWidth()
-                            );
-                            
-                            var availWidth = Math.floor(scope.calcAvailWidth() * 0.75);
+                                var collapseEls = document.querySelectorAll(
+                                    '.breadcrumb div:not(.anchor)');
 
-                            var focusWidth = Math.floor(scope.calcAvailWidth() * 0.25);
+                                var count = collapseEls.length;
 
-                            console.log(
-                                'availWidth',
-                                availWidth
-                            );
+                                console.log(
+                                    'calcAvailWidth',
+                                    scope.calcAvailWidth()
+                                );
 
-                            console.log(
-                                'focusWidth',
-                                focusWidth
-                            );
-                            
-                            scope.basis = {
-                                ancestor: availWidth / ancestorCount,
-                                focus: focusWidth
-                            }
+                                // scope.collapse(scope.calcAvailWidth())
 
-                            console.log(
-                                'scope.basis',
-                                scope.basis
-                            );
+                                var availWidth = Math.floor(scope.calcAvailWidth() * 0.75);
+
+                                var focusWidth = Math.floor(scope.calcAvailWidth() * 0.25);
+
+                                console.log(
+                                    'availWidth',
+                                    availWidth
+                                );
+
+                                console.log(
+                                    'focusWidth',
+                                    focusWidth
+                                );
+
+                                scope.basis = Math.floor(scope.calcAvailWidth() / count);
+
+                                // scope.basis = {
+                                //     ancestor: availWidth / ancestorCount,
+                                //     focus: focusWidth
+                                // }
+
+                                console.log(
+                                    'scope.basis',
+                                    scope.basis
+                                );
+
+                            }, 50)
 
                         };
 
@@ -128,7 +176,25 @@
 
                         });
 
+                        scope.$watch('metric', function (newVal) {
+
+                            scope.setBasis();
+
+                        });
+
+                        scope.$watch('practiceType', function (newVal) {
+
+                            scope.setBasis();
+
+                        });
+
                         scope.$watch('site', function (newVal) {
+
+                            scope.setBasis();
+
+                        });
+
+                        scope.$watch('program', function (newVal) {
 
                             scope.setBasis();
 
