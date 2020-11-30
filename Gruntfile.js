aade7691dda459403828de17d06b1308286c0144
rzthrun <rzthrun@gmail.com>
2020-11-30 15:06:44 -0500

    removedlogging import removelogging added to initconfig -- attempt to locate build error

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
+    grunt.loadNpmTasks("grunt-remove-logging");
 
     //
     // To get this working with the most recent 0.8.0 angular generator we needed to following the tips
@@ -36,9 +37,11 @@ module.exports = function(grunt) {
 
     // Define the configuration for all the tasks
     grunt.initConfig({
-
-
-
+        removelogging: {
+            dist: {
+                src: "dist/**/*.js" // Each file will be overwritten with the output!
+            }
+        },
         //
         // Environment Specific Variables
         //
@@ -472,7 +475,7 @@ module.exports = function(grunt) {
             'concurrent:server',
             'autoprefixer:server',
             'connect:livereload',
-            'watch',
+            'watch'
         ]);
     });
 
@@ -499,7 +502,7 @@ module.exports = function(grunt) {
         'copy:dist',
         'cssmin',
         'filerev',
-        'usemin',
+        'usemin'
     ];
 
     grunt.registerTask('build', buildTasks);
